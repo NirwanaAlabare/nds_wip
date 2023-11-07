753017bc68c0439a584508d8f1eaf22a05444348
Rizky <rizkyaditya2106@gmail.com>
2023-11-07 08:43:39 +0700

    fix searching error

--- app/Http/Controllers/CutPlanController.php
+++ app/Http/Controllers/CutPlanController.php
@@ -549,7 +549,6 @@ class CutPlanController extends Controller
                 filterColumn('form_info', function($query, $keyword) {
                     $query->whereHas('formCutInput', function($query) use ($keyword) {
                         $query->whereRaw("(
-                            form_cut_input.status != 'PENGERJAAN MARKER' OR
                             form_cut_input.no_form LIKE '%".$keyword."%' OR
                             form_cut_input.tgl_form_cut LIKE '%".$keyword."%'
                         )");
