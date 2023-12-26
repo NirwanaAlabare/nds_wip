f49311c0a9c419d6060da36f2c5c1f7233d6e2ec
it-nag <ramon@ptnag.com>
2023-12-26 10:49:15 +0700

    adjusting

--- resources/views/secondary-inhouse/secondary-inhouse.blade.php
+++ resources/views/secondary-inhouse/secondary-inhouse.blade.php
@@ -438,7 +438,7 @@
                     // let ket = $("#txtket").val();
                     // $.ajax({
                     //     type: "post",
-                    //     url: '{{ route('store-mut-karyawan') }}',
+                    //     url: '#',
                     //     data: {
                     //         txtenroll_id: txtenroll_id,
                     //         nm_line: nm_line,
