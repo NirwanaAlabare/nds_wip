7e87729f17b6aa31e7f22d7d6e9eb3673803b58f
Rizky <rizkyaditya2106@gmail.com>
2023-12-27 11:58:40 +0700

    adjusting

--- resources/views/spreading/create-spreading.blade.php
+++ resources/views/spreading/create-spreading.blade.php
@@ -130,7 +130,6 @@
                 </div>
             </div>
 
-
             <div class='row'>
                 <div class="col-md-6">
                     <div class="card card-primary">
@@ -383,7 +382,7 @@
                     document.getElementById('txt_cons_marker').value = response.cons_marker;
                     document.getElementById('hitungmarker').value = response.gelar_qty_balance ? response.gelar_qty_balance : response.gelar_qty;
                     document.getElementById('txtid_marker').value = response.kode;
-                    document.getElementById('tipe_form').value = (response.tipe_marker).replace(' marker', "");
+                    document.getElementById('tipe_form').value = capitalizeFirstLetter((response.tipe_marker).replace(' marker', ""));
                 },
                 error: function(request, status, error) {
                     alert(request.responseText);
