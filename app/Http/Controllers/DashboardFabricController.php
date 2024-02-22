f0a248852a9d80a069d815d8760d6ec48dd9cff8
it-nag <ramon@ptnag.com>
2024-02-22 09:59:04 +0700

    adjusting

--- app/Http/Controllers/DashboardFabricController.php
+++ app/Http/Controllers/DashboardFabricController.php
@@ -80,24 +80,27 @@ inner join mastersupplier mb on jd.id_buyer = mb.id_supplier where a.status = 'Y
     public function getdatarak(Request $request)
     {
 
-    $det_item = DB::connection('mysql_sb')->select("select buyer,no_ws,id_jo,id_item,goods_code,itemdesc,concat(count(no_barcode), ' ROLL') qty_roll from (select a.no_barcode,a.buyer,a.no_ws,a.id_jo,a.id_item,b.goods_code,b.itemdesc,a.qty from whs_sa_fabric a inner join masteritem b on b.id_item = a.id_item where a.qty > 0 and qty_mut is null and a.kode_lok = '" . $request->kode_lok . "'
+    $det_item = DB::connection('mysql_sb')->select("select no_barcode,buyer,no_ws,no_lot,no_roll,id_jo,id_item,goods_code,itemdesc,CONCAT(round(qty - COALESCE(qty_out,0),2),' ',unit) qty from (select a.no_barcode,a.buyer,a.no_ws,a.id_jo,a.id_item,b.goods_code,b.itemdesc, a.qty,a.unit,a.no_roll, a.no_lot from whs_sa_fabric a inner join masteritem b on b.id_item = a.id_item where a.qty > 0 and qty_mut is null and a.kode_lok = '" . $request->kode_lok . "'
 UNION
-select a.no_barcode,mb.supplier buyer,a.no_ws,a.id_jo,a.id_item,b.goods_code,b.itemdesc,a.qty_sj from whs_lokasi_inmaterial a 
+select a.no_barcode,mb.supplier buyer,a.no_ws,a.id_jo,a.id_item,b.goods_code,b.itemdesc,a.qty_sj,a.satuan,a.no_roll, a.no_lot from whs_lokasi_inmaterial a 
 inner join masteritem b on b.id_item = a.id_item
 inner join (select ac.id_buyer,ac.styleno,jd.id_jo, ac.kpno from jo_det jd inner join so on jd.id_so = so.id inner join act_costing ac on so.id_cost = ac.id where jd.cancel = 'N' group by id_cost order by id_jo asc) jd on a.id_jo = jd.id_jo
 inner join mastersupplier mb on jd.id_buyer = mb.id_supplier where a.status = 'Y' and qty_mutasi is null and a.kode_lok = '" . $request->kode_lok . "') a left JOIN
-(select id_roll,no_rak from whs_bppb_det where no_rak = '" . $request->kode_lok . "' and status = 'Y' GROUP BY id_roll) b on a.no_barcode = b.id_roll where id_roll is null GROUP BY id_jo,id_item");
+(select id_roll,no_rak,sum(qty_out) qty_out from whs_bppb_det where no_rak = '" . $request->kode_lok . "' and status = 'Y' GROUP BY id_roll) b on a.no_barcode = b.id_roll where round(qty - COALESCE(qty_out,0),2) > 0 GROUP BY no_barcode order by no_lot,no_roll asc");
 
         $html = '<div class="table-responsive">
             <table id="tableshow" class="table table-head-fixed table-bordered table-striped table-sm w-100">
                 <thead>
                     <tr>
-                        <th class="text-center" style="font-size: 0.6rem;width: 15%;">Buyer</th>
-                        <th class="text-center" style="font-size: 0.6rem;width: 15%;">No WS</th>
-                        <th class="text-center" style="font-size: 0.6rem;width: 10%;">Id JO</th>
-                        <th class="text-center" style="font-size: 0.6rem;width: 11%;">ID Item</th>
-                        <th class="text-center" style="font-size: 0.6rem;width: 24%;">Nama Barang</th>
-                        <th class="text-center" style="font-size: 0.6rem;width: 10%;">Qty</th>
+                        <th class="text-center" style="font-size: 0.6rem;">No Barcode</th>
+                        <th class="text-center" style="font-size: 0.6rem;">Buyer</th>
+                        <th class="text-center" style="font-size: 0.6rem;">No WS</th>
+                        <th class="text-center" style="font-size: 0.6rem;">No Lot</th>
+                        <th class="text-center" style="font-size: 0.6rem;">No Roll</th>
+                        <th class="text-center" style="font-size: 0.6rem;">ID JO</th>
+                        <th class="text-center" style="font-size: 0.6rem;">ID Item</th>
+                        <th class="text-center" style="font-size: 0.6rem;">Nama Barang</th>
+                        <th class="text-center" style="font-size: 0.6rem;">Qty</th>
                     </tr>
                 </thead>
                 <tbody>';
@@ -106,12 +109,15 @@ inner join mastersupplier mb on jd.id_buyer = mb.id_supplier where a.status = 'Y
             $x = 1;
         foreach ($det_item as $detitem) {
             $html .= ' <tr>
+                        <td> '.$detitem->no_barcode.'</td>
                         <td> '.$detitem->buyer.'</td>
                         <td> '.$detitem->no_ws.'</td>
+                        <td> '.$detitem->no_lot.'</td>
+                        <td> '.$detitem->no_roll.'</td>
                         <td> '.$detitem->id_jo.'</td>
                         <td> '.$detitem->id_item.'</td>
                         <td> '.$detitem->itemdesc.'</td>
-                        <td> '.$detitem->qty_roll.'</td>
+                        <td> '.$detitem->qty.'</td>
                        </tr>';
                        $x++;
         }
