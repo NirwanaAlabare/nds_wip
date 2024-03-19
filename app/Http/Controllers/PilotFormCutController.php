bebfd92d4fd10dde4aa112d91614a2c24faa6f02
Rizky <rizkyaditya2106@gmail.com>
2024-03-19 11:51:09 +0700

    adjust form

--- app/Http/Controllers/PilotFormCutController.php
+++ app/Http/Controllers/PilotFormCutController.php
@@ -410,16 +410,6 @@ class PilotFormCutController extends Controller
 
     public function getScannedItem($id = 0)
     {
-        $scannedItem = ScannedItem::where('id_roll', $id)->first();
-
-        if ($scannedItem) {
-            if (floatval($scannedItem->qty) > 0) {
-                return json_encode($scannedItem);
-            }
-
-            return json_encode(null);
-        }
-
         $newItem = DB::connection("mysql_sb")->select("
             SELECT
                 br.id_roll id_roll,
@@ -452,6 +442,16 @@ class PilotFormCutController extends Controller
                 LIMIT 1
         ");
         if ($newItem) {
+            $scannedItem = ScannedItem::where('id_roll', $id)->where('id_item', $newItem[0]->id_item)->first();
+
+            if ($scannedItem) {
+                if (floatval($scannedItem->qty) > 0) {
+                    return json_encode($scannedItem);
+                }
+
+                return json_encode(null);
+            }
+
             return json_encode($newItem ? $newItem[0] : null);
         }
 
@@ -489,8 +489,21 @@ class PilotFormCutController extends Controller
                 roll_qty AS DECIMAL ( 11, 3 )) > 0.000
                 LIMIT 1
         ");
+        if ($item) {
+            $scannedItem = ScannedItem::where('id_roll', $id)->where('id_item', $item[0]->id_item)->first();
 
-        return json_encode($item ? $item[0] : null);
+            if ($scannedItem) {
+                if (floatval($scannedItem->qty) > 0) {
+                    return json_encode($scannedItem);
+                }
+
+                return json_encode(null);
+            }
+
+            return json_encode($item ? $item[0] : null);
+        }
+
+        return  null;
     }
 
     public function getItem(Request $request) {
