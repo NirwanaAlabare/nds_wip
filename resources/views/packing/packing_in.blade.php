a59595c428a63d9ae29111eaf237add8efe2039d
Ramonez22 <ramonez.el.nino@gmail.com>
2024-06-27 13:38:27 +0700

    updt

--- resources/views/packing/packing_in.blade.php
+++ resources/views/packing/packing_in.blade.php
@@ -52,7 +52,7 @@
                 </div>
                 <label>Preview</label>
                 <div class="table-responsive">
-                    <table id="datatable_preview" class="table table-bordered table-sm w-100 table-hover display nowrap">
+                    <table id="datatable_preview" class="table table-bordered table-striped table-sm w-100 text-nowrap">
                         <thead>
                             <tr>
                                 <th>Line</th>
@@ -68,14 +68,11 @@
                         </thead>
                         <tfoot>
                             <tr>
-                                <th colspan="9"></th>
+                                <th colspan="6"></th>
+                                <th></th>
                                 <th> <input type = 'text' class="form-control form-control-sm" style="width:75px" readonly
                                         id = 'total_qty_chk'> </th>
                                 <th>PCS</th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
                             </tr>
                         </tfoot>
                     </table>
@@ -137,15 +134,6 @@
                             <th>Created At</th>
                         </tr>
                     </thead>
-                    <tfoot>
-                        <tr>
-                            <th colspan="9"></th>
-                            <th> <input type = 'text' class="form-control form-control-sm" style="width:75px" readonly
-                                    id = 'total_qty_chk'> </th>
-                            <th>PCS</th>
-                            <th></th>
-                        </tr>
-                    </tfoot>
                 </table>
             </div>
         </div>
@@ -193,49 +181,7 @@
             datatable_preview.ajax.reload();
         }
 
-        $('#datatable thead tr').clone(true).appendTo('#datatable thead');
-        $('#datatable thead tr:eq(1) th').each(function(i) {
-            var title = $(this).text();
-            $(this).html('<input type="text" class="form-control form-control-sm"/>');
-            $('input', this).on('keyup change', function() {
-                if (datatable.column(i).search() !== this.value) {
-                    datatable
-                        .column(i)
-                        .search(this.value)
-                        .draw();
-                }
-            });
-        });
-
-
         let datatable = $("#datatable").DataTable({
-
-            "footerCallback": function(row, data, start, end, display) {
-                var api = this.api(),
-                    data;
-
-                // converting to interger to find total
-                var intVal = function(i) {
-                    return typeof i === 'string' ?
-                        i.replace(/[\$,]/g, '') * 1 :
-                        typeof i === 'number' ?
-                        i : 0;
-                };
-
-                // computing column Total of the complete result
-                var sumTotal = api
-                    .column(9)
-                    .data()
-                    .reduce(function(a, b) {
-                        return intVal(a) + intVal(b);
-                    }, 0);
-
-                // Update footer by showing the total with the reference of the column index
-                $(api.column(0).footer()).html('Total');
-                $(api.column(9).footer()).html(sumTotal);
-            },
-
-
             ordering: false,
             processing: true,
             serverSide: true,
