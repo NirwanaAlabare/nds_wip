b953f432f01675bee49307b7aed4a55eeea03d05
Rizky <rizkyaditya2106@gmail.com>
2024-07-04 15:02:37 +0700

    adjusting

--- resources/views/track/worksheet/export/worksheet.blade.php
+++ resources/views/track/worksheet/export/worksheet.blade.php
@@ -36,62 +36,12 @@
     @endphp
     @foreach ($worksheet as $ws)
         <tr>
-            @if ($currentWorksheet != $ws->ws)
-                <td style="vertical-align: top;" rowspan="{{ $worksheet->where("ws", $ws->ws)->count() }}">{{ $ws->ws }}</td>
-
-                @php
-                    $currentWorksheet = $ws->ws;
-                    $currentStyle = null;
-                    $currentColor = null;
-                    $currentSize = null;
-                    $currentDest = null;
-                @endphp
-            @endif
-
-            @if ($currentWorksheet == $ws->ws && $currentStyle != $ws->styleno)
-                <td style="vertical-align: top;" rowspan="{{ $worksheet->where('ws', $ws->ws)->where("styleno", $ws->styleno)->count() }}">{{ $ws->styleno ? $ws->styleno : '-'  }}</td>
-
-                @php
-                    $currentStyle = $ws->styleno;
-                    $currentColor = null;
-                    $currentSize = null;
-                    $currentDest = null;
-                @endphp
-            @endif
-
-            @if ($currentWorksheet == $ws->ws && $currentStyle == $ws->styleno && $currentColor != $ws->color)
-                <td style="vertical-align: top;" rowspan="{{ $worksheet->where('ws', $ws->ws)->where('styleno', $ws->styleno)->where("color", $ws->color)->count() }}">{{ $ws->color ? $ws->color : '-'  }}</td>
-
-                @php
-                    $currentColor = $ws->color;
-                    $currentSize = null;
-                    $currentDest = null;
-                @endphp
-            @endif
-
-            @if ($currentWorksheet == $ws->ws && $currentStyle == $ws->styleno && $currentColor == $ws->color && $currentSize != $ws->size)
-                <td style="vertical-align: top;" rowspan="{{ $worksheet->where('ws', $ws->ws)->where('styleno', $ws->styleno)->where("color", $ws->color)->where("size", $ws->size)->count() }}">{{ $ws->size ? $ws->size : '-'  }}</td>
-
-                @php
-                    $currentSize = $ws->size;
-                    $currentDest = null;
-                @endphp
-            @endif
-
-            @if ($currentWorksheet == $ws->ws && $currentStyle == $ws->styleno && $currentColor == $ws->color && $currentSize == $ws->size && $currentDest != $ws->dest)
-                @php
-                    $currentRowspan = $worksheet->where('ws', $ws->ws)->where('styleno', $ws->styleno)->where("color", $ws->color)->where("size", $ws->size)->where("dest", $ws->dest)->count();
-                @endphp
-
-                <td style="vertical-align: top;" rowspan="{{ $currentRowspan }}">{{ $ws->dest ? $ws->dest : '-'  }}</td>
-                <td style="vertical-align: top;" rowspan="{{ $currentRowspan }}">{{ $ws->qty ? $ws->qty : '0'  }}</td>
-
-                @php
-                    $currentDest = $ws->dest;
-                    $currentRowspan = 0;
-                @endphp
-            @endif
-
+            <td style="vertical-align: top;">{{ $ws->ws }}</td>
+            <td style="vertical-align: top;">{{ $ws->styleno ? $ws->styleno : '-'  }}</td>
+            <td style="vertical-align: top;">{{ $ws->color ? $ws->color : '-'  }}</td>
+            <td style="vertical-align: top;">{{ $ws->size ? $ws->size : '-'  }}</td>
+            <td style="vertical-align: top;">{{ $ws->dest ? $ws->dest : '-'  }}</td>
+            <td style="vertical-align: top;">{{ $ws->qty ? $ws->qty : '0'  }}</td>
             <td style="vertical-align: top;">{{ $ws->panel ? $ws->panel : '-'  }}</td>
             <td style="vertical-align: top;">{{ $ws->total_ratio_marker ? $ws->total_ratio_marker : '0'  }}</td>
             <td style="vertical-align: top;">{{ $ws->total_gelar_marker ? $ws->total_gelar_marker : '0'  }}</td>
