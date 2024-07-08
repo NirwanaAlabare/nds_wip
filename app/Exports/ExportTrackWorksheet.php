c4748ded3178546e528b88467b81dc953c384bd0
Rizky <rizkyaditya2106@gmail.com>
2024-07-08 14:30:59 +0700

    adjust track module

--- app/Exports/ExportTrackWorksheet.php
+++ app/Exports/ExportTrackWorksheet.php
@@ -35,121 +35,121 @@ class ExportTrackWorksheet implements FromView, WithEvents, /*WithColumnWidths,*
     public function view(): View
     {
         $worksheet = DB::select("
-            select
-                DATE(master_sb_ws.tgl_kirim) tgl_kirim,
-                master_sb_ws.id_act_cost,
-                master_sb_ws.ws,
-                master_sb_ws.styleno,
-                master_sb_ws.color,
-                master_sb_ws.id_so_det,
-                master_sb_ws.size,
-                master_sb_ws.dest,
-                master_sb_ws.qty,
-                marker_track.kode,
-                marker_track.panel,
-                sum(marker_track.total_gelar_marker) total_gelar_marker,
-                sum(marker_track.total_ratio_marker) total_ratio_marker,
-                sum(marker_track.total_cut_marker) total_cut_marker,
-                sum(marker_track.total_lembar_form) total_lembar_form,
-                sum(marker_track.total_cut_form) total_cut_form,
-                sum(marker_track.total_stocker) total_stocker,
-                sum(marker_track.total_dc) total_dc,
-                sum(marker_track.total_sec) total_sec,
-                sum(marker_track.total_sec_in) total_sec_in
-            from
-                master_sb_ws
-            left join
-                (
-                select
-                    marker.id,
-                    marker.act_costing_id,
-                    marker.kode,
-                    marker.panel,
-                    marker_detail.so_det_id,
-                    marker.gelar_qty total_gelar_marker,
-                    marker_detail.ratio total_ratio_marker,
-                    marker_detail.cut_qty total_cut_marker,
-                    form_cut.qty_ply total_lembar_form,
-                    sum(marker_detail.ratio * form_cut.qty_ply) total_cut_form,
-                    sum(stocker.qty_ply) total_stocker,
-                    sum(stocker.dc_qty_ply) total_dc,
-                    sum(stocker.sec_qty_ply) total_sec,
-                    sum(stocker.sec_in_qty_ply) total_sec_in
-                from
-                    marker_input marker
-                left join
-                    (
-                        select
+            SELECT DATE
+                    ( master_sb_ws.tgl_kirim ) tgl_kirim,
+                    master_sb_ws.id_act_cost,
+                    master_sb_ws.ws,
+                    master_sb_ws.styleno,
+                    master_sb_ws.color,
+                    master_sb_ws.id_so_det,
+                    master_sb_ws.size,
+                    master_sb_ws.dest,
+                    master_sb_ws.qty,
+                    marker_track.kode,
+                    marker_track.panel,
+                    sum( marker_track.total_gelar_marker ) total_gelar_marker,
+                    sum( marker_track.total_ratio_marker ) total_ratio_marker,
+                    sum( marker_track.total_cut_marker ) total_cut_marker,
+                    sum( marker_track.total_lembar_form ) total_lembar_form,
+                    sum( marker_track.total_cut_form ) total_cut_form,
+                    sum( marker_track.total_stocker ) total_stocker,
+                    sum( marker_track.total_dc ) total_dc,
+                    sum( marker_track.total_sec ) total_sec,
+                    sum( marker_track.total_sec_in ) total_sec_in
+                FROM
+                    master_sb_ws
+                    LEFT JOIN (
+                    SELECT
+                        marker.id,
+                        marker.act_costing_id,
+                        marker.kode,
+                        marker.panel,
+                        marker_detail.so_det_id,
+                        marker.gelar_qty total_gelar_marker,
+                        marker_detail.ratio total_ratio_marker,
+                        marker_detail.cut_qty total_cut_marker,
+                        form_cut.qty_ply total_lembar_form,
+                        sum( marker_detail.ratio * form_cut.qty_ply ) total_cut_form,
+                        sum( stocker.qty_ply ) total_stocker,
+                        sum( stocker.dc_qty_ply ) total_dc,
+                        sum( stocker.sec_qty_ply ) total_sec,
+                        sum( stocker.sec_in_qty_ply ) total_sec_in
+                    FROM
+                        marker_input marker
+                        LEFT JOIN (
+                        SELECT
                             marker_input_detail.marker_id,
                             marker_input_detail.so_det_id,
                             marker_input_detail.size,
-                            sum(marker_input_detail.ratio) ratio,
-                            sum(marker_input_detail.cut_qty) cut_qty
-                        from
+                            sum( marker_input_detail.ratio ) ratio,
+                            sum( marker_input_detail.cut_qty ) cut_qty
+                        FROM
                             marker_input_detail
-                        where
+                        WHERE
                             marker_input_detail.ratio > 0
-                        group by
+                        GROUP BY
                             marker_id,
                             so_det_id
-                    ) marker_detail on marker_detail.marker_id = marker.id
-                left join
-                    (
-                        select
+                        ) marker_detail ON marker_detail.marker_id = marker.id
+                        LEFT JOIN (
+                        SELECT
                             form_cut_input.id,
                             form_cut_input.id_marker,
                             form_cut_input.no_form,
-                            sum(coalesce(form_cut_input.total_lembar, form_cut_input.qty_ply)) qty_ply
-                        from
+                            COALESCE ( form_cut_input.total_lembar, form_cut_input.qty_ply ) qty_ply
+                        FROM
                             form_cut_input
-                        where
-                            form_cut_input.qty_ply is not null and form_cut_input.id_marker is not null
-                        group by
-                            form_cut_input.id_marker
-                    ) form_cut on form_cut.id_marker = marker.kode
-                left join
-                    (
-                        select
+                        WHERE
+                            form_cut_input.qty_ply IS NOT NULL
+                            AND form_cut_input.id_marker IS NOT NULL
+                        ) form_cut ON form_cut.id_marker = marker.kode
+                        LEFT JOIN (
+                        SELECT
                             *
-                        from
-                        (
-                            select
+                        FROM
+                            (
+                            SELECT
                                 stocker_input.form_cut_id,
                                 stocker_input.part_detail_id,
                                 stocker_input.so_det_id,
-                                sum(coalesce(stocker_input.qty_ply_mod, stocker_input.qty_ply)) qty_ply,
-                                sum((dc_in_input.qty_awal - dc_in_input.qty_reject + dc_in_input.qty_replace)) dc_qty_ply,
-                                sum(secondary_in_input.qty_in) sec_qty_ply,
-                                sum(secondary_inhouse_input.qty_in) sec_in_qty_ply
-                            from
+                                sum(
+                                COALESCE ( stocker_input.qty_ply_mod, stocker_input.qty_ply )) qty_ply,
+                                sum((
+                                        dc_in_input.qty_awal - dc_in_input.qty_reject + dc_in_input.qty_replace
+                                    )) dc_qty_ply,
+                                sum( secondary_in_input.qty_in ) sec_qty_ply,
+                                sum( secondary_inhouse_input.qty_in ) sec_in_qty_ply
+                            FROM
                                 stocker_input
-                                inner join dc_in_input on dc_in_input.id_qr_stocker = stocker_input.id_qr_stocker
-                                left join secondary_in_input on secondary_in_input.id_qr_stocker = dc_in_input.id_qr_stocker
-                                left join secondary_inhouse_input on secondary_inhouse_input.id_qr_stocker = secondary_in_input.id_qr_stocker
-                            group by
+                                LEFT JOIN dc_in_input ON dc_in_input.id_qr_stocker = stocker_input.id_qr_stocker
+                                LEFT JOIN secondary_in_input ON secondary_in_input.id_qr_stocker = dc_in_input.id_qr_stocker
+                                LEFT JOIN secondary_inhouse_input ON secondary_inhouse_input.id_qr_stocker = secondary_in_input.id_qr_stocker
+                            GROUP BY
                                 stocker_input.form_cut_id,
                                 stocker_input.part_detail_id,
                                 stocker_input.so_det_id
-                        ) stocker
-                        group by
+                            ) stocker
+                        GROUP BY
                             stocker.form_cut_id,
                             stocker.so_det_id
-                    ) stocker on stocker.form_cut_id = form_cut.id and stocker.so_det_id = marker_detail.so_det_id
-                    group by
+                        ) stocker ON stocker.form_cut_id = form_cut.id
+                        AND stocker.so_det_id = marker_detail.so_det_id
+                    GROUP BY
                         marker.id,
                         marker_detail.so_det_id
-                ) marker_track on marker_track.act_costing_id = master_sb_ws.id_act_cost and marker_track.so_det_id = master_sb_ws.id_so_det
-                where
-                    MONTH( master_sb_ws.tgl_kirim ) = '".$this->month."' AND
-                    YEAR( master_sb_ws.tgl_kirim ) = '".$this->year."'
-                group by
+                    ) marker_track ON marker_track.act_costing_id = master_sb_ws.id_act_cost
+                    AND marker_track.so_det_id = master_sb_ws.id_so_det
+                WHERE
+                    MONTH ( master_sb_ws.tgl_kirim ) = '".$this->month."'
+                    AND YEAR ( master_sb_ws.tgl_kirim ) = '".$this->year."'
+                GROUP BY
                     master_sb_ws.id_so_det,
                     marker_track.panel
-                order by
+                ORDER BY
                     master_sb_ws.id_act_cost,
                     master_sb_ws.color,
-                    master_sb_ws.id_so_det,
-                    master_sb_ws.dest
+                    marker_track.panel,
+                    master_sb_ws.id_so_det
             ");
 
         $this->rowCount = count($worksheet) + 3;
--- routes/web.php
+++ routes/web.php
@@ -565,7 +565,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/worksheet/show-marker', 'wsMarker')->name('track-ws-marker');
         Route::get('/worksheet/show-marker-total', 'wsMarkerTotal')->name('track-ws-marker-total');
         Route::get('/worksheet/show-form', 'wsForm')->name('track-ws-form');
-        Route::get('/worksheet/show-form-total', 'wsForm')->name('track-ws-form-total');
+        Route::get('/worksheet/show-form-total', 'wsFormTotal')->name('track-ws-form-total');
         Route::get('/worksheet/show-roll', 'wsRoll')->name('track-ws-roll');
         Route::get('/worksheet/show-stocker', 'wsStocker')->name('track-ws-stocker');
     });
