21fadf4b400b15484c900ee6397e581511c4c0b1
it-nag <ramon@ptnag.com>
2024-07-26 17:17:05 +0700

    update

--- public/assets/dist/js/script.js
+++ public/assets/dist/js/script.js
@@ -176,8 +176,39 @@ function submitForm(e, evt) {
                 if (res.callback != '') {
                     eval(res.callback);
                 }
-            }
-            else {
+            } else if (res.status == 201) {
+                // $('.modal').modal('hide');
+
+                Swal.fire({
+                    icon: 'success',
+                    title: res.message,
+                    showCancelButton: false,
+                    showConfirmButton: true,
+                    confirmButtonText: 'Oke',
+                    timer: (res.status == 200 ? 5000 : 3000),
+                    timerProgressBar: true
+                }).then(() => {
+                    if (isNotNull(res.redirect)) {
+                        if (res.redirect != 'reload') {
+                            location.href = res.redirect;
+                        } else {
+                            location.reload();
+                        }
+                    } else {
+                        // location.reload();
+                    }
+                });
+
+                e.reset();
+                if (document.getElementsByClassName('select2')) {
+                    $(".select2").val('').trigger('change');
+                    $(".select2bs4").val('').trigger('change');
+                }
+
+                if (res.callback != '') {
+                    eval(res.callback);
+                }
+            } else {
                 for(let i = 0;i < res.errors; i++) {
                     document.getElementById(res.errors[i]).classList.add('is-invalid');
                     modified.push([res.errors[i], 'classList', 'remove(', "'is-invalid')"])
