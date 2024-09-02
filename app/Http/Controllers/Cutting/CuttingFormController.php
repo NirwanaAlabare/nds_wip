3a614fa950361843be24c60f0f15c20a2e851cb6
Rizky <rizkyaditya2106@gmail.com>
2024-09-02 14:25:41 +0700

    adjust cutting form

--- app/Http/Controllers/Cutting/CuttingFormController.php
+++ app/Http/Controllers/Cutting/CuttingFormController.php
@@ -307,7 +307,9 @@ class CuttingFormController extends Controller
                     return json_encode($scannedItem);
                 }
 
-                return json_encode(null);
+                $formCutInputDetail = FormCutInputDetail::where("id_roll", $id)->orderBy("updated_at", "desc")->first();
+
+                return "Roll sudah terpakai di form '".$formCutInputDetail->no_form_cut_input."'";
             }
 
             return json_encode($newItem ? $newItem[0] : null);
@@ -355,7 +357,9 @@ class CuttingFormController extends Controller
                     return json_encode($scannedItem);
                 }
 
-                return json_encode(null);
+                $formCutInputDetail = FormCutInputDetail::where("id_roll", $id)->orderBy("updated_at", "desc")->first();
+
+                return "Roll sudah terpakai di form '".$formCutInputDetail->no_form_cut_input."'";
             }
 
             return json_encode($item ? $item[0] : null);
