eaaa32994710d2673c4c2b0d66c78b6684831bc9
Rizky <rizkyaditya2106@gmail.com>
2024-09-04 16:08:43 +0700

    adjusting

--- app/Http/Livewire/TrackOrderOutput.php
+++ app/Http/Livewire/TrackOrderOutput.php
@@ -41,7 +41,7 @@ class TrackOrderOutput extends Component
         $this->dailyOrderOutputs = null;
         $this->loadingOrderOutput = false;
 
-        $this->dateFromFilter = date('Y-m-d', strtotime('-14 days'));
+        $this->dateFromFilter = date('Y-m-d', strtotime('-7 days'));
         $this->dateToFilter = date("Y-m-d");
 
         $this->outputType = null;
