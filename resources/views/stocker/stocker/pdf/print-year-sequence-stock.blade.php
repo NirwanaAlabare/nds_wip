44a94e9aba4acb1127186b44934b9b2323b01bf8
it-nag <ramon@ptnag.com>
2024-10-08 14:56:42 +0700

    adjust

--- resources/views/stocker/stocker/pdf/print-year-sequence-stock.blade.php
+++ resources/views/stocker/stocker/pdf/print-year-sequence-stock.blade.php
@@ -97,7 +97,7 @@
 
                 <th style='border: none;border-left: 1px solid;border-top: 1px solid;border-bottom: 1px solid;'>Range QR</th>
                 <td style='border: none;border-top: 1px solid;border-bottom: 1px solid;'>:</td>
-                <td style='border: none;border-top: 1px solid;border-bottom: 1px solid;border-right: 1px solid;'>{{ $range_akhir." - ".$range_awal }}</td>
+                <td style='border: none;border-top: 1px solid;border-bottom: 1px solid;border-right: 1px solid;'>{{ $range_awal." - ".$range_akhir }}</td>
             </tr>
         </table>
         <div class="clearfix">
