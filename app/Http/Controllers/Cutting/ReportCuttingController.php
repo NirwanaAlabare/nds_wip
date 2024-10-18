b2e0cc40f8798bead94f619dbcaa19411c142558
Rizky <rizkyaditya2106@gmail.com>
2024-10-18 17:34:57 +0700

    adjusting

--- app/Http/Controllers/Cutting/ReportCuttingController.php
+++ app/Http/Controllers/Cutting/ReportCuttingController.php
@@ -521,64 +521,124 @@ class ReportCuttingController extends Controller
         return Excel::download(new ExportDetailPemakaianKain($request->no_req, $request->id_item), 'Report Detail Pemakaian Kain.xlsx');
     }
 
-    public function create()
-    {
-        //
-    }
+    public function cuttingDaily(Request $request) {
+        if ($request->ajax()) {
+            $additionalQuery = "";
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+            if ($request->dateFrom) {
+                $additionalQuery .= " and COALESCE(DATE(waktu_selesai), DATE(waktu_mulai), tgl_form_cut) >= '".$request->dateFrom."'";
+            }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+            if ($request->dateTo) {
+                $additionalQuery .= " and COALESCE(DATE(waktu_selesai), DATE(waktu_mulai), tgl_form_cut) <= '".$request->dateTo."'";
+            }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+            $keywordQuery = "";
+            if ($request->search["value"]) {
+                $keywordQuery = "
+                    and (
+                        marker_cutting.tgl_form_cut like '%" . $request->search["value"] . "%' OR
+                        marker_cutting.meja like '%" . $request->search["value"] . "%' OR
+                        marker_cutting.buyer like '%" . $request->search["value"] . "%' OR
+                        marker_cutting.act_costing_ws like '%" . $request->search["value"] . "%' OR
+                        marker_cutting.style like '%" . $request->search["value"] . "%' OR
+                        marker_cutting.color like '%" . $request->search["value"] . "%' OR
+                        marker_cutting.notes like '%" . $request->search["value"] . "%'
+                    )
+                ";
+            }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+            $reportCutting = DB::select("
+                SELECT
+                    marker_cutting.tgl_form_cut,
+                    marker_cutting.meja,
+                    marker_cutting.act_costing_ws,
+                    marker_cutting.style,
+                    marker_cutting.color,
+                    marker_cutting.panel,
+                    SUM((marker_cutting.form_gelar * marker_cutting.ratio) + COALESCE(marker_cutting.diff, 0)) qty
+                FROM
+                    (
+                        SELECT
+                            marker_input.kode,
+                            GROUP_CONCAT(form_cut.no_form, form_cut.meja) no_form_meja,
+                            form_cut.id_meja,
+                            form_cut.meja,
+                            form_cut.tgl_form_cut,
+                            marker_input.buyer,
+                            marker_input.act_costing_id,
+                            marker_input.act_costing_ws,
+                            marker_input.style,
+                            marker_input.color,
+                            marker_input.panel,
+                            marker_input.cons_ws,
+                            marker_input.unit_panjang_marker unit,
+                            marker_input_detail.so_det_id,
+                            CONCAT(master_sb_ws.size, CASE WHEN master_sb_ws.dest != '-' AND master_sb_ws.dest IS NOT NULL THEN CONCAT(' - ', master_sb_ws.dest) ELSE '' END) size,
+                            marker_input_detail.ratio,
+                            COALESCE(marker_input.notes, form_cut.notes) notes,
+                            marker_input.gelar_qty marker_gelar,
+                            SUM(form_cut.qty_ply) spreading_gelar,
+                            SUM(COALESCE(form_cut.total_lembar, form_cut.detail)) form_gelar,
+                            SUM(modify_size_qty.difference_qty) diff
+                        FROM
+                            marker_input
+                            INNER JOIN
+                                marker_input_detail on marker_input_detail.marker_id = marker_input.id
+                            INNER JOIN
+                                master_sb_ws on master_sb_ws.id_so_det = marker_input_detail.so_det_id
+                            INNER JOIN
+                                (
+                                    SELECT
+                                        meja.id id_meja,
+                                        meja.`name` meja,
+                                        COALESCE(DATE(waktu_selesai), DATE(waktu_mulai), tgl_form_cut) tgl_form_cut,
+                                        form_cut_input.id_marker,
+                                        form_cut_input.no_form,
+                                        form_cut_input.qty_ply,
+                                        form_cut_input.total_lembar,
+                                        form_cut_input.notes,
+                                        SUM(form_cut_input_detail.lembar_gelaran) detail
+                                    FROM
+                                        form_cut_input
+                                        LEFT JOIN users meja ON meja.id = form_cut_input.no_meja
+                                        INNER JOIN form_cut_input_detail ON form_cut_input_detail.no_form_cut_input = form_cut_input.no_form
+                                    WHERE
+                                        form_cut_input.`status` != 'SPREADING'
+                                        AND form_cut_input.waktu_mulai is not null
+                                        ".$additionalQuery."
+                                    GROUP BY
+                                        form_cut_input.no_form,
+                                        form_cut_input.no_form
+                                ) form_cut on form_cut.id_marker = marker_input.kode
+                            LEFT JOIN
+                                modify_size_qty ON modify_size_qty.no_form = form_cut.no_form AND modify_size_qty.so_det_id = marker_input_detail.so_det_id
+                            where
+                                (marker_input.cancel IS NULL OR marker_input.cancel != 'Y')
+                                AND marker_input_detail.ratio > 0
+                            group by
+                                marker_input.id,
+                                marker_input_detail.so_det_id,
+                                form_cut.tgl_form_cut,
+                                form_cut.meja
+                    ) marker_cutting
+                GROUP BY
+                        marker_cutting.id_meja,
+                        marker_cutting.act_costing_id,
+                        marker_cutting.color,
+                        marker_cutting.panel,
+                        marker_cutting.tgl_form_cut
+                ORDER BY
+                        marker_cutting.id_meja,
+                        marker_cutting.panel,
+                        marker_cutting.act_costing_id,
+                        marker_cutting.color,
+                        marker_cutting.tgl_form_cut
+            ");
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+            return DataTables::of($reportCutting)->toJson();
+        }
+
+        return view('cutting.report.report-cutting-output-daily', ['page' => 'dashboard-cutting', "subPageGroup" => "cutting-report", "subPage" => "cutting"]);
     }
 }
