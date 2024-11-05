d978256141a1ed1f926c52363b62c67ff4156b56
Ramonez22 <ramonez.el.nino@gmail.com>
2024-11-05 11:42:12 +0700

    update

---
+++ app/Exports/Report_eff_new_export.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Sheet;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Carbon\Carbon;
+use DB;
+
+Sheet::macro('styleCells', function (Sheet $sheet, string $cellRange, array $style) {
+    $sheet->getDelegate()->getStyle($cellRange)->applyFromArray($style);
+});
+
+// class ExportLaporanPemakaian implements FromCollection
+// {
+//     /**
+//      * @return \Illuminate\Support\Collection
+//      */
+//     public function collection()
+//     {
+//         return Marker::all();
+//     }
+// }
+
+class Report_eff_new_export implements FromView, WithEvents, ShouldAutoSize
+{
+    use Exportable;
+
+
+    protected $tgl_awal, $tgl_akhir;
+
+    public function __construct($tgl_awal, $tgl_akhir)
+    {
+
+        $this->tgl_awal = $tgl_awal . ' 00:00:00';
+        $this->tgl_akhir = $tgl_akhir . ' 23:59:59';
+
+        $this->rowCount = 0;
+    }
+
+
+    public function view(): View
+
+    {
+        $data = DB::connection('mysql_sb')->select("SELECT
+    tgl_trans,
+    concat((DATE_FORMAT(tgl_trans,  '%d')), '-',left(DATE_FORMAT(tgl_trans,  '%M'),3),'-',DATE_FORMAT(tgl_trans,  '%Y')) tgl_trans_fix,
+    u.name sewing_line,
+    ac.kpno,
+    ms.Supplier buyer,
+    ac.styleno,
+    mp.smv,
+    mp.man_power,
+	ROUND(mp.man_power * ROUND(((sum(tot_output)/b.tot_output_line) * ROUND(TIME_TO_SEC(TIMEDIFF(TIMEDIFF(MAX(b.jam_akhir_input), MIN(jam_kerja_awal)), MAX(a.istirahat))) / 3600,2)),2) * 60 / mp.smv,0) set_target,
+    min(jam_kerja_awal)jam_kerja_awal,
+    b.jam_akhir_input,
+    max(istirahat) istirahat,
+    ROUND(TIME_TO_SEC(TIMEDIFF(TIMEDIFF(MAX(b.jam_akhir_input), MIN(jam_kerja_awal)), MAX(a.istirahat))) / 3600, 1) AS jam_kerja_line,
+    ROUND(((sum(tot_output)/b.tot_output_line) * ROUND(TIME_TO_SEC(TIMEDIFF(TIMEDIFF(MAX(b.jam_akhir_input), MIN(jam_kerja_awal)), MAX(a.istirahat))) / 3600,2)),2) jam_kerja_act,
+    ROUND(mp.man_power * ROUND(((sum(tot_output)/b.tot_output_line) * ROUND(TIME_TO_SEC(TIMEDIFF(TIMEDIFF(MAX(b.jam_akhir_input), MIN(jam_kerja_awal)), MAX(a.istirahat))) / 3600,2)),2) *60,0) min_available,
+    ROUND(sum(tot_output) * mp.smv,0) min_prod,
+    concat(ROUND(ROUND(sum(tot_output) * mp.smv,0) / ROUND(mp.man_power * ROUND(((sum(tot_output)/b.tot_output_line) * ROUND(TIME_TO_SEC(TIMEDIFF(TIMEDIFF(MAX(b.jam_akhir_input), MIN(jam_kerja_awal)), MAX(a.istirahat))) / 3600,2)),2) *60,0) * 100,2), ' %') eff,
+    sum(tot_output) output,
+    b.tot_output_line,
+    sd.price,
+    ac.curr,
+    acm.price cm_price,
+    round(sum(tot_output) * acm.price,2) earning
+    from
+    (
+    select
+    date(updated_at)tgl_trans,
+    so_det_id,
+    master_plan_id,
+    count(so_det_id) tot_output,
+    time(max(a.updated_at)) jam_akhir_input,
+    case
+    when time(max(a.updated_at)) >= '12:00:00' and time(max(a.updated_at)) <= '18:44:59' THEN '01:00:00'
+    when time(max(a.updated_at)) <= '12:00:00'  THEN '00:00:00'
+    when time(max(a.updated_at)) >= '18:45:00'  THEN '01:30:00'
+    END as istirahat,
+    created_by
+    from output_rfts a
+    where updated_at >= '$this->tgl_awal' and updated_at <= '$this->tgl_akhir'
+    group by master_plan_id, created_by
+    ) a
+    inner join master_plan mp on a.master_plan_id = mp.id
+    inner join so_det sd on a.so_det_id = sd.id
+    inner join so on sd.id_so = so.id
+    inner join act_costing ac on so.id_cost = ac.id
+    inner join user_sb_wip u on a.created_by = u.id
+    inner join mastersupplier ms on ac.id_buyer = ms.Id_Supplier
+    left join
+    (
+    select * from act_costing_mfg where id_item = '8' group by id_act_cost
+    ) acm on ac.id = acm.id_act_cost
+    left join
+    (
+    select created_by,max(time(a.updated_at)) jam_akhir_input, count(so_det_id) tot_output_line
+    from output_rfts a where updated_at >= '$this->tgl_awal' and updated_at <= '$this->tgl_akhir' group by created_by
+    ) b on a.created_by = b.created_by
+    group by ac.kpno, ac.styleno,u.name, a.tgl_trans
+    order by tgl_trans asc, name asc, kpno asc
+
+        ");
+
+
+        $this->rowCount = count($data) + 4;
+
+
+        return view(
+            'sewing.export.report_efficiency_new_export',
+            [
+                'data' => $data,
+                'tgl_awal_n' => $this->tgl_awal,
+                'tgl_akhir_n' => $this->tgl_akhir
+            ]
+        );
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => [self::class, 'afterSheet']
+        ];
+    }
+
+
+
+    public static function afterSheet(AfterSheet $event)
+    {
+
+        $event->sheet->styleCells(
+            'A4:P' . $event->getConcernable()->rowCount,
+            [
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                        'color' => ['argb' => '000000'],
+                    ],
+                ],
+            ]
+        );
+    }
+}
---
+++ resources/views/sewing/export/report_efficiency_new_export.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<table class="table">
+    <tr>
+        <td colspan='9'>EFISIENSI TOTAL</td>
+    </tr>
+    <tr>
+        {{-- <td colspan='12'>{{ date('d-M-Y', strtotime($from)) }} - {{ date('d-M-Y', strtotime($to)) }}
+        </td> --}}
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+    <thead>
+        <tr>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Tgl. Trans</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Line</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">WS</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Buyer</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Style</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">SMV</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">MP</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Mins Avail</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Target</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Output</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Currency</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">CM Price</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Earning</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Mins. Prod</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Efficiency</th>
+            <th style="background-color: lightblue;border:1px solid black;font-weight:bold">Jam Kerja Aktual</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php
+            $no = 1;
+        @endphp
+        @foreach ($data as $item)
+            <tr>
+                <td>{{ $item->tgl_trans_fix }}</td>
+                <td>{{ $item->sewing_line }}</td>
+                <td>{{ $item->kpno }}</td>
+                <td>{{ $item->buyer }}</td>
+                <td>{{ $item->styleno }}</td>
+                <td>{{ $item->smv }}</td>
+                <td>{{ $item->man_power }}</td>
+                <td>{{ $item->min_available }}</td>
+                <td>{{ $item->set_target }}</td>
+                <td>{{ $item->output }}</td>
+                <td>{{ $item->curr }}</td>
+                <td>{{ $item->cm_price }}</td>
+                <td>{{ $item->earning }}</td>
+                <td>{{ $item->min_prod }}</td>
+                <td>{{ $item->eff }}</td>
+                <td>{{ $item->jam_kerja_act }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+
+</table>
+
+</html>
