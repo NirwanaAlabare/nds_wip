de9082eaa8449693ee3f1209b16ae0917c8f38e2
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2024-12-24 11:19:08 +0700

    report output cutting qty synchronize

--- app/Http/Controllers/Cutting/ReportCuttingController.php
+++ app/Http/Controllers/Cutting/ReportCuttingController.php
@@ -52,97 +52,65 @@ class ReportCuttingController extends Controller
 
             $reportCutting = DB::select("
                 SELECT
-                    marker_cutting.tgl_form_cut,
-                    marker_cutting.meja,
-                    marker_cutting.buyer,
-                    marker_cutting.act_costing_ws,
-                    marker_cutting.style,
-                    marker_cutting.color,
-                    marker_cutting.panel,
-                    marker_cutting.cons_ws,
-                    marker_cutting.unit,
-                    marker_cutting.so_det_id,
-                    marker_cutting.size,
-                    COALESCE(marker_cutting.notes, '-') notes,
-                    SUM(marker_cutting.marker_gelar * marker_cutting.ratio) marker_gelar,
-                    SUM(marker_cutting.spreading_gelar  * marker_cutting.ratio) spreading_gelar,
-                    SUM((marker_cutting.form_gelar * marker_cutting.ratio) + COALESCE(marker_cutting.diff, 0)) form_gelar,
-                    SUM(COALESCE(marker_cutting.diff, 0)) form_diff
+                    marker_input.kode,
+                    form_cut.no_form,
+                    form_cut.meja,
+                    form_cut.tgl_form_cut,
+                    marker_input.buyer,
+                    marker_input.act_costing_id,
+                    marker_input.act_costing_ws,
+                    marker_input.style,
+                    marker_input.color,
+                    marker_input.panel,
+                    marker_input.cons_ws,
+                    marker_input.unit_panjang_marker unit,
+                    marker_input_detail.so_det_id,
+                    CONCAT(master_sb_ws.size, CASE WHEN master_sb_ws.dest != '-' AND master_sb_ws.dest IS NOT NULL THEN CONCAT(' - ', master_sb_ws.dest) ELSE '' END) size,
+                    marker_input_detail.ratio,
+                    COALESCE(marker_input.notes, form_cut.notes) notes,
+                    marker_input.gelar_qty marker_gelar,
+                    SUM(form_cut.qty_ply) spreading_gelar,
+                    SUM(COALESCE(form_cut.total_lembar, form_cut.detail)) form_gelar,
+                    SUM(modify_size_qty.difference_qty) form_diff
                 FROM
-                    (
-                        SELECT
-                            marker_input.kode,
-                            form_cut.no_form,
-                            form_cut.meja,
-                            form_cut.tgl_form_cut,
-                            marker_input.buyer,
-                            marker_input.act_costing_id,
-                            marker_input.act_costing_ws,
-                            marker_input.style,
-                            marker_input.color,
-                            marker_input.panel,
-                            marker_input.cons_ws,
-                            marker_input.unit_panjang_marker unit,
-                            marker_input_detail.so_det_id,
-                            CONCAT(master_sb_ws.size, CASE WHEN master_sb_ws.dest != '-' AND master_sb_ws.dest IS NOT NULL THEN CONCAT(' - ', master_sb_ws.dest) ELSE '' END) size,
-                            marker_input_detail.ratio,
-                            COALESCE(marker_input.notes, form_cut.notes) notes,
-                            marker_input.gelar_qty marker_gelar,
-                            SUM(form_cut.qty_ply) spreading_gelar,
-                            SUM(COALESCE(form_cut.total_lembar, form_cut.detail)) form_gelar,
-                            SUM(modify_size_qty.difference_qty) diff
-                        FROM
-                            marker_input
-                            INNER JOIN
-                                marker_input_detail on marker_input_detail.marker_id = marker_input.id
-                            INNER JOIN
-                                master_sb_ws on master_sb_ws.id_so_det = marker_input_detail.so_det_id
-                            INNER JOIN
-                                (
-                                    SELECT
-                                        meja.`name` meja,
-                                        COALESCE(DATE(form_cut_input.waktu_selesai), DATE(form_cut_input.waktu_mulai), DATE(form_cut_input.tgl_input)) tgl_form_cut,
-                                        form_cut_input.id_marker,
-                                        form_cut_input.id,
-                                        form_cut_input.no_form,
-                                        form_cut_input.qty_ply,
-                                        form_cut_input.total_lembar,
-                                        form_cut_input.notes,
-                                        SUM(form_cut_input_detail.lembar_gelaran) detail
-                                    FROM
-                                        form_cut_input
-                                        LEFT JOIN users meja ON meja.id = form_cut_input.no_meja
-                                        INNER JOIN form_cut_input_detail ON form_cut_input_detail.form_cut_id = form_cut_input.id
-                                    WHERE
-                                        form_cut_input.`status` = 'SELESAI PENGERJAAN'
-                                        AND form_cut_input.waktu_mulai is not null
-                                        ".$additionalQuery."
-                                    GROUP BY
-                                        form_cut_input.id
-                                ) form_cut on form_cut.id_marker = marker_input.kode
-                            LEFT JOIN
-                                modify_size_qty ON modify_size_qty.form_cut_id = form_cut.id AND modify_size_qty.so_det_id = marker_input_detail.so_det_id
-                            where
-                                (marker_input.cancel IS NULL OR marker_input.cancel != 'Y')
-                                AND marker_input_detail.ratio > 0
-                            group by
-                                marker_input.id,
-                                marker_input_detail.so_det_id,
-                                form_cut.tgl_form_cut
-                    ) marker_cutting
-                GROUP BY
-                    marker_cutting.act_costing_id,
-                    marker_cutting.color,
-                    marker_cutting.panel,
-                    marker_cutting.so_det_id,
-                    marker_cutting.tgl_form_cut
-                ORDER BY
-                    marker_cutting.panel,
-                    marker_cutting.act_costing_id,
-                    marker_cutting.color,
-                    marker_cutting.so_det_id,
-                    marker_cutting.tgl_form_cut
-                ");
+                    marker_input
+                    INNER JOIN
+                        marker_input_detail on marker_input_detail.marker_id = marker_input.id
+                    INNER JOIN
+                        master_sb_ws on master_sb_ws.id_so_det = marker_input_detail.so_det_id
+                    INNER JOIN
+                        (
+                            SELECT
+                                meja.`name` meja,
+                                COALESCE(DATE(form_cut_input.waktu_selesai), DATE(form_cut_input.waktu_mulai), DATE(form_cut_input.tgl_input)) tgl_form_cut,
+                                form_cut_input.id_marker,
+                                form_cut_input.id,
+                                form_cut_input.no_form,
+                                form_cut_input.qty_ply,
+                                form_cut_input.total_lembar,
+                                form_cut_input.notes,
+                                SUM(form_cut_input_detail.lembar_gelaran) detail
+                            FROM
+                                form_cut_input
+                                LEFT JOIN users meja ON meja.id = form_cut_input.no_meja
+                                INNER JOIN form_cut_input_detail ON form_cut_input_detail.form_cut_id = form_cut_input.id
+                            WHERE
+                                form_cut_input.`status` = 'SELESAI PENGERJAAN'
+                                AND form_cut_input.waktu_mulai is not null
+                                ".$additionalQuery."
+                            GROUP BY
+                                form_cut_input.id
+                        ) form_cut on form_cut.id_marker = marker_input.kode
+                    LEFT JOIN
+                        modify_size_qty ON modify_size_qty.form_cut_id = form_cut.id AND modify_size_qty.so_det_id = marker_input_detail.so_det_id
+                where
+                    (marker_input.cancel IS NULL OR marker_input.cancel != 'Y')
+                    AND marker_input_detail.ratio > 0
+                group by
+                    marker_input.id,
+                    marker_input_detail.so_det_id,
+                    form_cut.tgl_form_cut
+            ");
 
             return DataTables::of($reportCutting)->toJson();
         }
@@ -255,14 +223,14 @@ class ReportCuttingController extends Controller
                             ) form_cut on form_cut.id_marker = marker_input.kode
                         LEFT JOIN
                             modify_size_qty ON modify_size_qty.form_cut_id = form_cut.id AND modify_size_qty.so_det_id = marker_input_detail.so_det_id
-                        where
-                            (marker_input.cancel IS NULL OR marker_input.cancel != 'Y')
-                            AND marker_input_detail.ratio > 0
-                            ".$additionalQuery1."
-                        group by
-                            marker_input.id,
-                            marker_input_detail.so_det_id,
-                            form_cut.tgl_form_cut
+                    where
+                        (marker_input.cancel IS NULL OR marker_input.cancel != 'Y')
+                        AND marker_input_detail.ratio > 0
+                        ".$additionalQuery1."
+                    group by
+                        marker_input.id,
+                        marker_input_detail.so_det_id,
+                        form_cut.tgl_form_cut
                 ) marker_cutting
             ");
 
