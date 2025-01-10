db22cf69a8525ae4270c36a989ecfa671ba2b279
Ramonez22 <ramonez.el.nino@gmail.com>
2025-01-10 13:45:40 +0700

    update report fg

--- app/Http/Controllers/FGStokLaporanController.php
+++ app/Http/Controllers/FGStokLaporanController.php
@@ -166,4 +166,65 @@ class FGStokLaporanController extends Controller
             return DataTables::of($data_input)->toJson();
         }
     }
+
+
+    public function export_excel_mutasi_fg_stock(Request $request)
+    {
+        $tgl_awal = $request->from;
+        $tgl_akhir = $request->to;
+        $data = DB::select("SELECT
+        ROW_NUMBER() OVER (ORDER BY buyer ASC, color ASC, ms.urutan ASC) AS no_urut,
+                mt.id_so_det,
+                sum(qty_awal) qty_awal,
+                sum(qty_in) qty_in,
+                sum(qty_out) qty_out,
+                sum(qty_awal) + sum(qty_in) - sum(qty_out) saldo_akhir,
+                grade,
+                lokasi,
+                no_carton,
+                buyer,
+                color,
+                m.size,
+                ws,
+                brand,
+                styleno,
+                m.product_group,
+                m.product_item,
+                m.dest
+                from
+                (
+                    select id_so_det,sum(qty_in) - sum(qty_out) qty_awal,'0' qty_in,'0' qty_out, grade, lokasi, no_carton
+                    from
+                    (
+                    select id_so_det,sum(qty) qty_in,'0' qty_out,grade, lokasi, no_carton
+                    from fg_stok_bpb
+                    where tgl_terima < '$tgl_awal'
+                    group by id_so_det, grade, lokasi, no_carton
+                    UNION
+                    select id_so_det,'0' qty_in,sum(qty_out) qty_out,grade, lokasi, no_carton
+                    from fg_stok_bppb
+                    where tgl_pengeluaran < '$tgl_awal'
+                    group by id_so_det, grade, lokasi, no_carton
+                    ) sa
+                    group by id_so_det, grade, lokasi, no_carton
+                union
+                select id_so_det,'0' qty_awal,sum(qty) qty_in,'0' qty_out,grade, lokasi, no_carton
+                from fg_stok_bpb
+                where tgl_terima >= '$tgl_awal' and tgl_terima <= '$tgl_akhir'
+                group by id_so_det, grade, lokasi, no_carton
+                union
+                select id_so_det,'0' qty_awal,'0' qty_in,sum(qty_out) qty_out,grade, lokasi, no_carton
+                from fg_stok_bppb
+                where tgl_pengeluaran >= '$tgl_awal' and tgl_pengeluaran <= '$tgl_akhir'
+                group by id_so_det, grade, lokasi, no_carton
+                )
+                mt
+                left join master_sb_ws m on mt.id_so_det = m.id_so_det
+                left join master_size_new ms on m.size = ms.size
+                group by mt.id_so_det, grade, lokasi, no_carton
+                order by buyer asc, color asc, ms.urutan asc
+              ");
+
+        return response()->json($data);
+    }
 }
