e6d163d980f2959e7f4b88b38f535a5afdd86070
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-01-10 11:10:05 +0700

    fix report output query for employee list

--- app/Http/Livewire/ReportOutput.php
+++ app/Http/Livewire/ReportOutput.php
@@ -418,10 +418,12 @@ class ReportOutput extends Component
             $masterPlanDateFilter = " between '".$this->dateFrom."' and '".$this->dateTo."'";
             $masterPlanDateFilter1 = " between '".date('Y-m-d', strtotime('-7 days', strtotime($this->dateFrom)))."' and '".$this->dateTo."'";
             $outputFilter = " between '".$this->dateFrom." 00:00:00' and '".$this->dateTo." 23:59:59'";
+            $leaderDate = $this->dateTo;
         } else {
             $masterPlanDateFilter = " = '".$this->date."'";
             $masterPlanDateFilter1 = " between '".date('Y-m-d', strtotime('-7 days', strtotime($this->date)))."' and '".$this->date."'";
             $outputFilter = " between '".$this->date." 00:00:00' and '".$this->date." 23:59:59'";
+            $leaderDate = $this->date;
         }
 
         if (($this->range == "custom" && date('Y-m-d H:i:s') >= $this->dateFrom.' 16:00:00') || date('Y-m-d H:i:s') >= $this->date.' 16:00:00') {
@@ -432,6 +434,7 @@ class ReportOutput extends Component
 
         if ($this->group == 'line') {
             $lines = MasterPlan::selectRaw("
+                    userpassword.line_id,
                     output_leader_line.employee_nik leader_nik,
                     output_leader_line.employee_name leader_name,
                     MAX(act_costing.kpno) kpno,
@@ -455,9 +458,9 @@ class ReportOutput extends Component
                     MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( DATE(master_plan.tgl_plan) ) ELSE 0 END) tgl_plan,
                     GREATEST(IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( rfts.last_rft ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( defects.last_defect ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( reworks.last_rework ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( rejects.last_reject ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)) ) latest_output")->
                 leftJoin("userpassword", "userpassword.username", "=", "master_plan.sewing_line")->
-                leftJoin("output_leader_line", function ($join) {
+                leftJoin("output_leader_line", function ($join) use ($leaderDate) {
                     $join->on("output_leader_line.line_id", "=", "userpassword.line_id");
-                    $join->on("output_leader_line.tanggal", "=", "master_plan.tgl_plan");
+                    $join->on("output_leader_line.tanggal", "=", DB::raw("'".$leaderDate."'"));
                 })->
                 leftJoin("act_costing", "act_costing.id", "=", "master_plan.id_ws")->
                 join("so", "so.id_cost", "=", "act_costing.id")->
@@ -508,6 +511,7 @@ class ReportOutput extends Component
                 $masterPlans = MasterPlan::all();
 
                 $orders = MasterPlan::selectRaw("
+                    userpassword.line_id,
                     output_leader_line.employee_nik leader_nik,
                     output_leader_line.employee_name leader_name,
                     master_plan.id_ws,
@@ -532,9 +536,9 @@ class ReportOutput extends Component
                     GREATEST(IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( rfts.last_rft ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( defects.last_defect ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( reworks.last_rework ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( rejects.last_reject ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)) ) latest_output
                 ")->
                 leftJoin("userpassword", "userpassword.username", "=", "master_plan.sewing_line")->
-                leftJoin("output_leader_line", function ($join) {
+                leftJoin("output_leader_line", function ($join) use ($leaderDate) {
                     $join->on("output_leader_line.line_id", "=", "userpassword.line_id");
-                    $join->on("output_leader_line.tanggal", "=", "master_plan.tgl_plan");
+                    $join->on("output_leader_line.tanggal", "=", DB::raw("'".$leaderDate."'"));
                 })->
                 leftJoin("act_costing", "act_costing.id", "=", "master_plan.id_ws")->
                 join("so", "so.id_cost", "=", "act_costing.id")->
@@ -578,6 +582,7 @@ class ReportOutput extends Component
                 $masterPlans = MasterPlan::all();
 
                 $orders = MasterPlan::selectRaw("
+                    userpassword.line_id,
                     output_leader_line.employee_nik leader_nik,
                     output_leader_line.employee_name leader_name,
                     MAX(master_plan.id_ws) as id_ws,
@@ -602,9 +607,9 @@ class ReportOutput extends Component
                     GREATEST(IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( rfts.last_rft ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( defects.last_defect ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( reworks.last_rework ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)), IFNULL(MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( rejects.last_reject ) ELSE 0 END), MAX(CASE WHEN master_plan.tgl_plan ".$selectFilter." THEN ( master_plan.tgl_plan ) ELSE 0 END)) ) latest_output
                 ")->
                 leftJoin("userpassword", "userpassword.username", "=", "master_plan.sewing_line")->
-                leftJoin("output_leader_line", function ($join) {
+                leftJoin("output_leader_line", function ($join) use ($leaderDate) {
                     $join->on("output_leader_line.line_id", "=", "userpassword.line_id");
-                    $join->on("output_leader_line.tanggal", "=", "master_plan.tgl_plan");
+                    $join->on("output_leader_line.tanggal", "=", DB::raw("'".$leaderDate."'"));
                 })->
                 leftJoin("act_costing", "act_costing.id", "=", "master_plan.id_ws")->
                 join("so", "so.id_cost", "=", "act_costing.id")->
