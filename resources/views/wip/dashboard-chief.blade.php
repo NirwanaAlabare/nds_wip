1a43e16274b44f9265226d12ef3c37c47ad1c818
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-01-31 17:41:08 +0700

    add dashboard chief sewing

---
+++ resources/views/wip/dashboard-chief.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.index', ["navbar" => false, "footer" => false, "containerFluid" => true])
+
+@section('custom-link')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+
+    <style>
+        .dataTables_wrapper .dataTables_processing {
+            position: absolute;
+            top: 50px !important;
+            /* background: #FFFFCC;
+            border: 1px solid black; */
+            border-radius: 3px;
+            font-weight: bold;
+        }
+    </style>
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('custom-script')
+    <!-- DataTables & Plugins -->
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+
+    <!-- Select2 -->
+    <script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
+    <script>
+        $('.select2').select2()
+        $('.select2bs4').select2({
+            theme: 'bootstrap4'
+        })
+    </script>
+@endsection
