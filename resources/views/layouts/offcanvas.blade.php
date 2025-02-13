8f7e57ca059c7b6be9db0214b5936037193ef005
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-02-13 10:25:03 +0700

    adjust offcanvas variable

--- resources/views/layouts/offcanvas.blade.php
+++ resources/views/layouts/offcanvas.blade.php
@@ -2,7 +2,7 @@
     <div class="offcanvas-header">
         <h5 class="offcanvas-title text-sb fw-bold" id="offcanvasExampleLabel">
             <i class="fa-solid fa-circle-user mt-1"></i>
-            {{ strtoupper(auth()->user()->name) }}
+            {{ strtoupper((auth() && auth()->user() ? auth()->user()->name : null)) }}
         </h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
@@ -10,24 +10,24 @@
         <div>
             <div class="card bg-light">
                 <div class="card-body">
-                    <form action="{{ route('update-user', ["id" => auth()->user()->id]) }}" method="post" onsubmit="submitForm(this, event);">
+                    <form action="{{ route('update-user', ["id" => (auth() && auth()->user() ? auth()->user()->id : null)]) }}" method="post" onsubmit="submitForm(this, event);">
                         @csrf
                         <input type="hidden" name="_method" value="PUT">
 
                         <div class="mb-3">
                             <label><small>Name</small></label>
-                            <input type="text" class="form-control form-control-sm" name="name" value="{{ auth()->user()->name }}" readonly>
+                            <input type="text" class="form-control form-control-sm" name="name" value="{{ (auth() && auth()->user() ? auth()->user()->name : null) }}" readonly>
                         </div>
                         <div class="mb-3">
                             <label><small>Username</small></label>
-                            <input type="text" class="form-control form-control-sm" name="username" value="{{ auth()->user()->username }}" readonly>
+                            <input type="text" class="form-control form-control-sm" name="username" value="{{ (auth() && auth()->user() ? auth()->user()->username : null) }}" readonly>
                         </div>
-                        @if (auth()->user()->type == 'admin')
+                        @if ((auth() && auth()->user() ? auth()->user()->type : null) == 'admin')
                             <div class="mb-3 d-none">
                                 <label><small>Unlock Token</small></label>
                                 <div class="input-group input-group-sm">
-                                    <input type="text" class="form-control" name="unlock_token" id="unlock_token" value="{{ auth()->user()->unlock_token }}" readonly>
-                                    <button type="button" class="btn btn-sb" onclick="generateToken('{{ auth()->user()->id }}', '{{ route('generate-unlock-token') }}')">Generate</button>
+                                    <input type="text" class="form-control" name="unlock_token" id="unlock_token" value="{{ (auth() && auth()->user() ? auth()->user()->unlock_token : null) }}" readonly>
+                                    <button type="button" class="btn btn-sb" onclick="generateToken('{{ (auth() && auth()->user() ? auth()->user()->id : null) }}', '{{ route('generate-unlock-token') }}')">Generate</button>
                                 </div>
                             </div>
                         @endif
