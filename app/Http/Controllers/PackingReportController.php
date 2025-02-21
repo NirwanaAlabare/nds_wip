59d19da2bc7cdb4642c0d935d3a9fdf7c18a45a5
Ramonez22 <ramonez.el.nino@gmail.com>
2025-02-21 09:11:08 +0700

    update

--- app/Http/Controllers/PackingReportController.php
+++ app/Http/Controllers/PackingReportController.php
@@ -557,10 +557,9 @@ order by buyer asc, kpno asc, styleno asc, color asc, msn.urutan asc
         $data = DB::connection('mysql_sb')->select("SELECT
                     ac.kpno,
                     ms.supplier buyer,
-                    ac.styleno,
+                    sd.styleno_prod,
                     sd.color,
                     sd.size,
-                    sd.dest,
 					sum(sa_pck_line_awal) - sum(sa_trf_gmt_awal) sa_pck_line_awal,
 					sum(qty_in_pck_line) qty_in_pck_line,
 					sum(input_rework_sewing) input_rework_sewing,
@@ -821,7 +820,7 @@ inner join signalbit_erp.so so on sd.id_so = so.id
 inner join signalbit_erp.act_costing ac on so.id_cost = ac.id
 inner join signalbit_erp.mastersupplier ms on ac.id_buyer = ms.Id_Supplier
 left join signalbit_erp.master_size_new msn on sd.size = msn.size
-group by so_det_id
+group by kpno, buyer, styleno_prod, color, size
 order by buyer asc, kpno asc, styleno asc, color asc, msn.urutan asc
 
               ");
