1c2e6c7e714667ff07688b9c49135589dd28a9d8
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-03-17 17:06:41 +0700

    adjust chief sewing range export

--- resources/views/sewing/export/chief-sewing-range-export.blade.php
+++ resources/views/sewing/export/chief-sewing-range-export.blade.php
@@ -1,9 +1,9 @@
 <table>
     <tr>
-        <td colspan="{{ $chiefPerformance->sortBy("tanggal")->groupBy("tanggal")->count() }}" style="text-align: center;">Tanggal Export : {{ $from || $to ? $from." - ".$to : 'All Day' }}</td>
+        <td style="text-align: center;">Tanggal Export : {{ $from || $to ? $from." - ".$to : 'All Day' }}</td>
     </tr>
     <tr>
-        <td colspan="{{ $chiefPerformance->sortBy("tanggal")->groupBy("tanggal")->count() }}" style="text-align: center; font-weight: 800;">Chief Performance Range</td>
+        <td style="text-align: center; font-weight: 800;">Chief Performance Range</td>
     </tr>
     @if ($chiefPerformance && $chiefPerformance->count() > 0)
         <tr>
@@ -31,10 +31,30 @@
                     <th style="font-weight: 800;">Efficiency</th>
                 </tr>
             <?php
-                foreach ($chiefPerformance->groupBy("chief_nik") as $key => $chief) {
+                $chiefGroups = $chiefPerformance->groupBy("chief_nik");
+
+                $chiefGroup = $chiefGroups->map(function ($group) {
+                    return [
+                        'chief_nik' => $group->first()->chief_nik, // opposition_id is constant inside the same group, so just take the first or whatever.
+                        'chief_name' => $group->first()->chief_name, // opposition_id is constant inside the same group, so just take the first or whatever.
+                        'total_rft' => $group->sum('rft'),
+                        'total_output' => $group->sum('output'),
+                        'total_mins_prod' => $group->sum('mins_prod'),
+                        'total_mins_avail' => $group->sum('mins_avail'),
+                    ];
+                });
+
+                $sortedChiefGroup = $chiefGroup->sort(function ($a, $b) {
+                    $eff_a = $a['total_mins_avail'] > 0 ? $a['total_mins_prod']/$a['total_mins_avail']*100 : '-';
+                    $eff_b = $b['total_mins_avail'] > 0 ? $b['total_mins_prod']/$b['total_mins_avail']*100 : '-';
+
+                    return $eff_b - $eff_a;
+                });
+
+                foreach ($sortedChiefGroup as $chief) {
                     ?>
                         <tr>
-                            <td style="text-align: left;vertical-align: top;">{{ $chief->first()->chief_name }}</td>
+                            <td style="text-align: left;vertical-align: top;">{{ $chief['chief_name'] }}</td>
                             @php
                                 $sumRft = 0;
                                 $sumOutput = 0;
@@ -43,10 +63,10 @@
                             @endphp
                             @foreach ($chiefPerformance->sortBy("tanggal")->groupBy("tanggal") as $dailyDate)
                                 @php
-                                    $thisRft = $chiefPerformance->where('chief_nik', $key)->where('tanggal', $dailyDate->first()->tanggal)->sum("rft");
-                                    $thisOutput = $chiefPerformance->where('chief_nik', $key)->where('tanggal', $dailyDate->first()->tanggal)->sum("output");
-                                    $thisMinsProd = $chiefPerformance->where('chief_nik', $key)->where('tanggal', $dailyDate->first()->tanggal)->sum("mins_prod");
-                                    $thisMinsAvail = $chiefPerformance->where('chief_nik', $key)->where('tanggal', $dailyDate->first()->tanggal)->sum("mins_avail");
+                                    $thisRft = $chiefPerformance->where('chief_nik', $chief['chief_nik'])->where('tanggal', $dailyDate->first()->tanggal)->sum("rft");
+                                    $thisOutput = $chiefPerformance->where('chief_nik', $chief['chief_nik'])->where('tanggal', $dailyDate->first()->tanggal)->sum("output");
+                                    $thisMinsProd = $chiefPerformance->where('chief_nik', $chief['chief_nik'])->where('tanggal', $dailyDate->first()->tanggal)->sum("mins_prod");
+                                    $thisMinsAvail = $chiefPerformance->where('chief_nik', $chief['chief_nik'])->where('tanggal', $dailyDate->first()->tanggal)->sum("mins_avail");
 
                                     $sumRft += $thisRft;
                                     $sumOutput += $thisOutput;
@@ -61,10 +81,10 @@
                                 </td>
                             @endforeach
                             <td style="font-weight: 800;">
-                                {{ ($sumRft > 0 ? round($sumRft/$sumOutput*100, 2) : 0) }} %
+                                {{ ($chief['total_output'] > 0 ? round($chief['total_rft']/$chief['total_output']*100, 2) : 0) }} %
                             </td>
                             <td style="font-weight: 800;">
-                                {{ ($sumMinsAvail > 0 ? round($sumMinsProd/$sumMinsAvail*100, 2) : 0) }} %
+                                {{ ($chief['total_mins_avail']  > 0 ? round($chief['total_mins_prod']/$chief['total_mins_avail']*100, 2) : 0) }} %
                             </td>
                         </tr>
                     <?php
