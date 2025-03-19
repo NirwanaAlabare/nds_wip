3496d2469de1f128ff7d2b863fe3f3e5aa710e55
it-nag <ramon@ptnag.com>
2025-03-19 09:02:19 +0700

    adjusting

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -535,7 +535,7 @@
                                 </li>
                                 <li>
                                     <a href="{{ route('dashboard-chief-sewing-range', [date("Y-m-d"), date("Y-m-d")]) }}" class="dropdown-item {{ $routeName == 'dashboard-chief-sewing-range' ? 'active' : '' }}">
-                                        Chief Performance Range <i class="fa-solid fa-file"></i>
+                                        Chief Performance <i class="fa-solid fa-file"></i>
                                     </a>
                                 </li>
                             @endrole
