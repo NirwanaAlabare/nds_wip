9f1369ff947f1858d46597ed625bd27d36a2f4dd
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-04-25 16:29:26 +0700

    adjust top defect export excel

--- app/Exports/Sewing/TopDefectExport.php
+++ app/Exports/Sewing/TopDefectExport.php
@@ -26,6 +26,7 @@ class TopDefectExport implements FromView, ShouldAutoSize, WithCharts, WithTitle
     protected $sewingLine;
     protected $rowCount;
     protected $colAlphabet;
+    protected $colAlphabetSub;
     protected $colAlphabetChartStart;
     protected $colAlphabetChartEnd;
 
@@ -49,6 +50,9 @@ class TopDefectExport implements FromView, ShouldAutoSize, WithCharts, WithTitle
         $topDefect = collect(DB::connection("mysql_sb")->select($this->query));
 
         $this->rowCount = $topDefect->groupBy("grouping")->count();
+        $this->rowCountLine = $topDefect->groupBy("line_grouping")->count();
+        $this->rowCountStyle = $topDefect->groupBy("style_grouping")->count();
+
         $alphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
         $colCount = $topDefect->groupBy("tanggal")->count()+3;
         if ($colCount > (count($alphabets)-1)) {
@@ -57,6 +61,15 @@ class TopDefectExport implements FromView, ShouldAutoSize, WithCharts, WithTitle
             $this->colAlphabet = $alphabets[$colStack-1].$alphabets[($colStackModulo > 0 ? $colStackModulo - 1 : $colStackModulo)];
         } else {
             $this->colAlphabet = $alphabets[$colCount];
+        }
+
+        $colAlphabetSub = $colCount-2;
+        if ($colAlphabetSub > (count($alphabets)-1)) {
+            $colStack = floor($colAlphabetSub/(count($alphabets)-1));
+            $colStackModulo = $colAlphabetSub%(count($alphabets)-1);
+            $this->colAlphabetSub = $alphabets[$colStack-1].$alphabets[($colStackModulo > 0 ? $colStackModulo - 1 : $colStackModulo)];
+        } else {
+            $this->colAlphabetSub = $alphabets[$colAlphabetSub];
         }
 
         $colAlphabetChartStart = $colCount+3;
@@ -68,7 +81,7 @@ class TopDefectExport implements FromView, ShouldAutoSize, WithCharts, WithTitle
             $this->colAlphabetChartStart = $alphabets[$colAlphabetChartStart];
         }
 
-        $colAlphabetChartEnd = $colCount+23;
+        $colAlphabetChartEnd = $colCount+($this->rowCount > 23 ? 23 : $this->rowCount);
         if ($colAlphabetChartEnd > (count($alphabets)-1)) {
             $colStack = floor($colAlphabetChartEnd/(count($alphabets)-1));
             $colStackModulo = $colAlphabetChartEnd%(count($alphabets)-1);
@@ -97,7 +110,6 @@ class TopDefectExport implements FromView, ShouldAutoSize, WithCharts, WithTitle
         $valuesDefect = [];
 
         for ($i = 0; $i < $this->rowCount; $i++) {
-            // Defect
             array_push($labelsDefect,
                 new DataSeriesValues('String', '$A$'.($i+7).':$D$'.($i+7).'', null, 5)
             );
@@ -111,7 +123,6 @@ class TopDefectExport implements FromView, ShouldAutoSize, WithCharts, WithTitle
             );
         }
 
-        // Eff
         $seriesDefect = new DataSeries(DataSeries::TYPE_LINECHART, DataSeries::GROUPING_STANDARD, range(0, count($valuesDefect) - 1), $labelsDefect, $categoriesDefect, $valuesDefect);
         $plotDefect   = new PlotArea(null, [$seriesDefect]);
 
@@ -119,31 +130,64 @@ class TopDefectExport implements FromView, ShouldAutoSize, WithCharts, WithTitle
         $chartDefect  = new Chart('Line Style Defect Chart', new Title('Defect Chart'), $legendDefect, $plotDefect);
 
         $chartDefect->setTopLeftPosition($this->colAlphabetChartStart.'6');
-        $chartDefect->setBottomRightPosition($this->colAlphabetChartEnd.'36');
+        $chartDefect->setBottomRightPosition($this->colAlphabetChartEnd.($this->rowCount > 36 ? 36 : $this->rowCount));
 
-        return $chartDefect;
+        // Defect Line
+        $labelsDefectLine = [];
+        $categoriesDefectLine = [];
+        $valuesDefectLine = [];
 
-        // $labels = [new DataSeriesValues('String', '$A$7:$F$7', null, 1)];
-        // $categories = [new DataSeriesValues('String', '$G$6:$G$6', null, 3)];
-        // $values = [new DataSeriesValues('Number', '$G$10:$G$10', null, 3)];
+        for ($i = 0; $i < $this->rowCountLine; $i++) {
+            array_push($labelsDefectLine,
+                new DataSeriesValues('String', '$A$'.($i+$this->rowCount+10).':$B$'.($i+$this->rowCount+10).'', null, 5)
+            );
 
-        // $series = new DataSeries(
-        //     DataSeries::TYPE_LINECHART,
-        //     DataSeries::GROUPING_STANDARD,
-        //     [0],
-        //     $labels,
-        //     $categories,
-        //     $values
-        // );
+            array_push($categoriesDefectLine,
+                new DataSeriesValues('String', '$C$'.($this->rowCount+9).':$'.$this->colAlphabetSub.'$'.($this->rowCount+9), null, 5)
+            );
 
-        // $plotArea = new PlotArea(null, [$series]);
-        // $legend = new Legend();
-        // $title = new Title('Test Chart');
+            array_push($valuesDefectLine,
+                new DataSeriesValues('Number', '$C$'.($i+$this->rowCount+10).':$'.$this->colAlphabetSub.'$'.($i+$this->rowCount+10).'', null, 5)
+            );
+        }
 
-        // $chart = new Chart('test_chart', $title, $legend, $plotArea);
-        // $chart->setTopLeftPosition('F2');
-        // $chart->setBottomRightPosition('O15');
+        $seriesDefectLine = new DataSeries(DataSeries::TYPE_LINECHART, DataSeries::GROUPING_STANDARD, range(0, count($valuesDefectLine) - 1), $labelsDefectLine, $categoriesDefectLine, $valuesDefectLine);
+        $plotDefectLine   = new PlotArea(null, [$seriesDefectLine]);
 
-        // return $chart;
+        $legendDefectLine = new Legend();
+        $chartDefectLine  = new Chart('Line Defect Chart', new Title('Line Defect Chart'), $legendDefectLine, $plotDefectLine);
+
+        $chartDefectLine->setTopLeftPosition($this->colAlphabetChartStart.($this->rowCount+9));
+        $chartDefectLine->setBottomRightPosition($this->colAlphabetChartEnd.($this->rowCount+9+($this->rowCountLine > 36 ? 36 : $this->rowCountLine)));
+
+        // Defect Style
+        $labelsDefectStyle = [];
+        $categoriesDefectStyle = [];
+        $valuesDefectStyle = [];
+
+        for ($i = 0; $i < $this->rowCountStyle; $i++) {
+            array_push($labelsDefectStyle,
+                new DataSeriesValues('String', '$A$'.($i+$this->rowCount+$this->rowCountLine+12).':$B$'.($i+$this->rowCount+$this->rowCountLine+12).'', null, 5)
+            );
+
+            array_push($categoriesDefectStyle,
+                new DataSeriesValues('String', '$C$'.($this->rowCount+$this->rowCountLine+11).':$'.$this->colAlphabetSub.'$'.($this->rowCount+$this->rowCountLine+11), null, 5)
+            );
+
+            array_push($valuesDefectStyle,
+                new DataSeriesValues('Number', '$C$'.($i+$this->rowCount+$this->rowCountLine+12).':$'.$this->colAlphabetSub.'$'.($i+$this->rowCount+$this->rowCountLine+12).'', null, 5)
+            );
+        }
+
+        $seriesDefectStyle = new DataSeries(DataSeries::TYPE_LINECHART, DataSeries::GROUPING_STANDARD, range(0, count($valuesDefectStyle) - 1), $labelsDefectStyle, $categoriesDefectStyle, $valuesDefectStyle);
+        $plotDefectStyle   = new PlotArea(null, [$seriesDefectStyle]);
+
+        $legendDefectStyle = new Legend();
+        $chartDefectStyle  = new Chart('Style Defect Chart', new Title('Style Defect Chart'), $legendDefectStyle, $plotDefectStyle);
+
+        $chartDefectStyle->setTopLeftPosition($this->colAlphabetChartStart.($this->rowCount+$this->rowCountLine+11));
+        $chartDefectStyle->setBottomRightPosition($this->colAlphabetChartEnd.($this->rowCount+$this->rowCountLine+11+($this->rowCountStyle > 36 ? 36 : $this->rowCountStyle)));
+
+        return [$chartDefect, $chartDefectLine, $chartDefectStyle];
     }
 }
--- resources/views/sewing/export/top-defect-export.blade.php
+++ resources/views/sewing/export/top-defect-export.blade.php
@@ -35,7 +35,7 @@
             ['total_defect', 'desc'],
         ])->values();
     @endphp
-    {{-- EFF --}}
+    {{-- Line Style --}}
     <tr>
         <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Line</th>
         <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Style</th>
@@ -94,5 +94,129 @@
         <th style="font-weight: 800;border: 1px solid #000;" data-format="0">
             {{ $topDefect->sum("total_defect") }}
         </th>
+    </tr>
+
+    <tr></tr>
+
+    @php
+        $lineGroups = $topDefect->groupBy("line_grouping");
+
+        $lineGroup = $lineGroups->map(function ($group) {
+            return [
+                'line_grouping' => $group->first()->line_grouping,
+                'sewing_line' => $group->first()->sewing_line,
+                'defect_type' => $group->first()->defect_type,
+                'total_defect' => $group->sum('total_defect')
+            ];
+        });
+
+        $sortedLineGroup = $lineGroup->sortBy([
+            ['total_defect', 'desc'],
+        ])->values();
+    @endphp
+    {{-- Line --}}
+    <tr>
+        <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Line</th>
+        <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Defect Type</th>
+        <?php
+            if ( $topDefect && $topDefect->count() > 0 ) {
+                foreach ($topDefect->sortBy("tanggal")->groupBy("tanggal") as $dailyDate) {
+                    ?>
+                        <th style="font-weight: 800; text-align: center;border: 1px solid #000;">{{ date_format(date_create($dailyDate->first()->tanggal), "d-m-Y") }}</th>
+                    <?php
+                }
+        ?>
+        <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Grand Total</th>
+    </tr>
+        <?php
+                foreach ($sortedLineGroup as $line) {
+                    ?>
+                        <tr>
+                            <td style="border: 1px solid #000;">{{ $line['sewing_line'] }}</td>
+                            <td style="border: 1px solid #000;">{{ $line['defect_type'] }}</td>
+                            @foreach ($topDefect->sortBy("tanggal")->groupBy("tanggal") as $dailyDate)
+                                @php
+                                    $thisDefect = $topDefect->where('line_grouping', $line['line_grouping'])->where('tanggal', $dailyDate->first()->tanggal)->sum("total_defect");
+                                @endphp
+                                <td style="border: 1px solid #000;" data-format="0">
+                                    {{ $thisDefect }}
+                                </td>
+                            @endforeach
+                            <td style="font-weight: 800;vertical-align: middle;border: 1px solid #000;" data-format="0">
+                                {{ $line['total_defect'] }}
+                            </td>
+                        </tr>
+                    <?php
+                }
+            } else {
+                ?>
+                    <tr>
+                        <td style="text-align:center;">Data tidak ditemukan</td>
+                    </tr>
+                <?php
+            }
+        ?>
+    <tr>
+    </tr>
+
+    @php
+        $styleGroup = $topDefect->groupBy("style_grouping");
+
+        $styleGroup = $styleGroup->map(function ($group) {
+            return [
+                'style_grouping' => $group->first()->style_grouping,
+                'style' => $group->first()->style,
+                'defect_type' => $group->first()->defect_type,
+                'total_defect' => $group->sum('total_defect')
+            ];
+        });
+
+        $sortedStyleGroup = $styleGroup->sortBy([
+            ['total_defect', 'desc'],
+        ])->values();
+    @endphp
+    {{-- Style --}}
+    <tr>
+        <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Style</th>
+        <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Defect Type</th>
+        <?php
+            if ( $topDefect && $topDefect->count() > 0 ) {
+                foreach ($topDefect->sortBy("tanggal")->groupBy("tanggal") as $dailyDate) {
+                    ?>
+                        <th style="font-weight: 800; text-align: center;border: 1px solid #000;">{{ date_format(date_create($dailyDate->first()->tanggal), "d-m-Y") }}</th>
+                    <?php
+                }
+        ?>
+        <th style="font-weight: 800;text-align: center;vertical-align: middle;border: 1px solid #000;">Grand Total</th>
+    </tr>
+        <?php
+                foreach ($sortedStyleGroup as $style) {
+                    ?>
+                        <tr>
+                            <td style="border: 1px solid #000;">{{ $style['style'] }}</td>
+                            <td style="border: 1px solid #000;">{{ $style['defect_type'] }}</td>
+                            @foreach ($topDefect->sortBy("tanggal")->groupBy("tanggal") as $dailyDate)
+                                @php
+                                    $thisDefect = $topDefect->where('style_grouping', $style['style_grouping'])->where('tanggal', $dailyDate->first()->tanggal)->sum("total_defect");
+                                @endphp
+                                <td style="border: 1px solid #000;" data-format="0">
+                                    {{ $thisDefect }}
+                                </td>
+                            @endforeach
+                            <td style="font-weight: 800;vertical-align: middle;border: 1px solid #000;" data-format="0">
+                                {{ $style['total_defect'] }}
+                            </td>
+                        </tr>
+                    <?php
+                }
+            } else {
+                ?>
+                    <tr>
+                        <td style="text-align:center;">Data tidak ditemukan</td>
+                    </tr>
+                <?php
+            }
+        ?>
+    <tr>
     </tr>
 </table>
