98bc523f3b3cd6c4dd8bd9c2a2faf55cae2fe201
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-06-25 16:11:10 +0700

    adjust track cutting query

--- app/Http/Livewire/TrackCuttingOutput.php
+++ app/Http/Livewire/TrackCuttingOutput.php
@@ -24,6 +24,7 @@ class TrackCuttingOutput extends Component
     public $dateToFilter;
 
     public $colorFilter;
+    public $panelFilter;
     public $mejaFilter;
     public $sizeFilter;
 
@@ -45,6 +46,7 @@ class TrackCuttingOutput extends Component
         $this->dateToFilter = date("Y-m-d");
 
         $this->colorFilter = null;
+        $this->panelFilter = null;
         $this->mejaFilter = null;
         $this->sizeFilter = null;
 
@@ -55,6 +57,7 @@ class TrackCuttingOutput extends Component
     public function clearFilter()
     {
         $this->colorFilter = null;
+        $this->panelFilter = null;
         $this->mejaFilter = null;
         $this->sizeFilter = null;
     }
@@ -82,6 +85,7 @@ class TrackCuttingOutput extends Component
         $dateFilter = " AND COALESCE(DATE(waktu_selesai), DATE(waktu_mulai), tgl_form_cut) between '".$this->dateFromFilter."' and '".$this->dateToFilter."' ";
         $orderFilterQuery = $this->selectedOrder ? " AND marker_cutting.act_costing_id = '".$this->selectedOrder."' " : "";
         $colorFilterQuery = $this->colorFilter ? " AND marker_cutting.color = '".$this->colorFilter."' " : "";
+        $panelFilterQuery = $this->panelFilter ? " AND marker_cutting.panel = '".$this->panelFilter."' " : "";
         $mejaFilterQuery = $this->mejaFilter ? " AND marker_cutting.id_meja = '".$this->mejaFilter."' " : "";
         $sizeFilterQuery = $this->groupBy == "size" && $this->sizeFilter ? "AND marker_cutting.size = '".$this->mejaFilter."'" : "";
 
@@ -186,7 +190,7 @@ class TrackCuttingOutput extends Component
                 marker_input.style,
                 marker_input.color,
                 marker_input.panel
-                ".($this->groupBy == 'size' ? ', marker_input_detail.so_det_id, marker_input_detail.size' : '')."
+                ".($this->groupBy == 'size' ? ", marker_input_detail.so_det_id, CONCAT(master_sb_ws.size, CASE WHEN master_sb_ws.dest != '-' AND master_sb_ws.dest IS NOT NULL THEN CONCAT(' - ', master_sb_ws.dest) ELSE '' END) size" : '')."
             ")->
             leftJoin(
             DB::raw("
@@ -217,6 +221,7 @@ class TrackCuttingOutput extends Component
             leftJoin("users as meja", "meja.id", "=", "form_cut_input.no_meja")->
             leftJoin("marker_input", "marker_input.kode", "=", "form_cut_input.id_marker")->
             leftJoin("marker_input_detail", function ($join) { $join->on('marker_input.id', '=', 'marker_input_detail.marker_id'); $join->on('marker_input_detail.ratio', '>', DB::raw('0')); })->
+            leftJoin("master_sb_ws", "marker_input_detail.so_det_id", "=", "master_sb_ws.id_so_det")->
             whereRaw("
                 form_cut_input.`status` = 'SELESAI PENGERJAAN'
                 AND form_cut_input.id_marker is not null
@@ -231,11 +236,14 @@ class TrackCuttingOutput extends Component
             if ($this->colorFilter) {
                 $dailyOrderGroupSql->where('marker_input.color', $this->colorFilter);
             }
+            if ($this->panelFilter) {
+                $dailyOrderGroupSql->where('marker_input.panel', $this->panelFilter);
+            }
             if ($this->mejaFilter) {
                 $dailyOrderGroupSql->where('form_cut_input.no_meja', $this->mejaFilter);
             }
             if ($this->groupBy == "size" && $this->sizeFilter) {
-                $dailyOrderGroupSql->where('marker_input_detail.size', $this->sizeFilter);
+                $dailyOrderGroupSql->where('master_sb_ws.size', $this->sizeFilter);
             }
             if ($this->selectedOrder) {
                 $dailyOrderGroupSql->where("marker_input.act_costing_id", $this->selectedOrder);
@@ -245,58 +253,58 @@ class TrackCuttingOutput extends Component
                 orderBy("marker_input.act_costing_id", "asc")->
                 orderBy("marker_input.style", "asc")->
                 orderBy("marker_input.color", "asc")->
-                orderByRaw("form_cut_input.no_meja asc, marker_input.panel, marker_input_detail.so_det_id asc, marker_input_detail.size asc");
+                orderByRaw("form_cut_input.no_meja asc, marker_input.panel, marker_input_detail.so_det_id asc, master_sb_ws.size asc");
 
             $this->dailyOrderGroup = $dailyOrderGroupSql->get();
 
         $dailyOrderOutputSql = collect(
-                DB::select("
-                    SELECT
-                        marker_cutting.tgl_form_cut tanggal,
-                        marker_cutting.id_meja,
-                        UPPER(marker_cutting.meja) meja,
-                        marker_cutting.act_costing_ws ws,
-                        marker_cutting.style,
-                        marker_cutting.color,
-                        marker_cutting.panel,
-                        ".($this->groupBy == 'size' ? ' marker_cutting.so_det_id, marker_cutting.size, ' : '')."
-                        SUM((marker_cutting.form_gelar * marker_cutting.ratio) + COALESCE(marker_cutting.diff, 0)) qty
-                    FROM
-                        (
-                            SELECT
-                                marker_input.kode,
-                                GROUP_CONCAT(form_cut.no_form, form_cut.meja) no_form_meja,
-                                form_cut.id_meja,
-                                form_cut.meja,
-                                form_cut.tgl_form_cut,
-                                marker_input.buyer,
-                                marker_input.act_costing_id,
-                                marker_input.act_costing_ws,
-                                marker_input.style,
-                                marker_input.color,
-                                marker_input.panel,
-                                marker_input.cons_ws,
-                                marker_input.unit_panjang_marker unit,
-                                marker_input_detail.so_det_id,
-                                master_sb_ws.size,
-                                marker_input_detail.ratio,
-                                COALESCE(marker_input.notes, form_cut.notes) notes,
-                                marker_input.gelar_qty marker_gelar,
-                                SUM(form_cut.qty_ply) spreading_gelar,
-                                SUM(COALESCE(form_cut.total_lembar, form_cut.detail)) form_gelar,
-                                SUM(modify_size_qty.difference_qty) diff
-                            FROM
+            DB::select("
+                SELECT
+                    marker_cutting.tgl_form_cut tanggal,
+                    marker_cutting.id_meja,
+                    UPPER(marker_cutting.meja) meja,
+                    marker_cutting.act_costing_ws ws,
+                    marker_cutting.style,
+                    marker_cutting.color,
+                    marker_cutting.panel,
+                    ".($this->groupBy == 'size' ? ' marker_cutting.so_det_id, marker_cutting.size, ' : '')."
+                    SUM((marker_cutting.form_gelar * marker_cutting.ratio) + COALESCE(marker_cutting.diff, 0)) qty
+                FROM
+                    (
+                        SELECT
+                            marker_input.kode,
+                            form_cut.no_form,
+                            form_cut.id_meja,
+                            form_cut.meja,
+                            form_cut.tgl_form_cut,
+                            marker_input.buyer,
+                            marker_input.act_costing_id,
+                            marker_input.act_costing_ws,
+                            marker_input.style,
+                            marker_input.color,
+                            marker_input.panel,
+                            marker_input.cons_ws,
+                            marker_input.unit_panjang_marker unit,
+                            marker_input_detail.so_det_id,
+                            CONCAT(master_sb_ws.size, CASE WHEN master_sb_ws.dest != '-' AND master_sb_ws.dest IS NOT NULL THEN CONCAT(' - ', master_sb_ws.dest) ELSE '' END) size,
+                            marker_input_detail.ratio,
+                            COALESCE(marker_input.notes, form_cut.notes) notes,
+                            marker_input.gelar_qty marker_gelar,
+                            SUM(form_cut.qty_ply) spreading_gelar,
+                            SUM(COALESCE(form_cut.detail, form_cut.total_lembar)) form_gelar,
+                            SUM(modify_size_qty.difference_qty) diff
+                        FROM
                             marker_input
                             INNER JOIN
                                 marker_input_detail on marker_input_detail.marker_id = marker_input.id
-                            LEFT JOIN
+                            INNER JOIN
                                 master_sb_ws on master_sb_ws.id_so_det = marker_input_detail.so_det_id
                             INNER JOIN
                                 (
                                     SELECT
-                                        meja.id id_meja,
+                                        form_cut_input.no_meja id_meja,
                                         meja.`name` meja,
-                                        COALESCE(DATE(waktu_selesai), DATE(waktu_mulai), tgl_form_cut) tgl_form_cut,
+                                        COALESCE(DATE(form_cut_input.waktu_selesai), DATE(form_cut_input.waktu_mulai), DATE(form_cut_input.tgl_input)) tgl_form_cut,
                                         form_cut_input.id_marker,
                                         form_cut_input.id,
                                         form_cut_input.no_form,
@@ -310,6 +318,7 @@ class TrackCuttingOutput extends Component
                                         INNER JOIN form_cut_input_detail ON form_cut_input_detail.form_cut_id = form_cut_input.id
                                     WHERE
                                         form_cut_input.`status` = 'SELESAI PENGERJAAN'
+                                        AND form_cut_input.waktu_mulai is not null
                                         ".$dateFilter."
                                         ".($this->mejaFilter ? "AND form_cut_input.no_meja = '".$this->mejaFilter."'" :  "")."
                                     GROUP BY
@@ -317,34 +326,34 @@ class TrackCuttingOutput extends Component
                                 ) form_cut on form_cut.id_marker = marker_input.kode
                             LEFT JOIN
                                 modify_size_qty ON modify_size_qty.form_cut_id = form_cut.id AND modify_size_qty.so_det_id = marker_input_detail.so_det_id
-                            where
-                                (marker_input.cancel IS NULL OR marker_input.cancel != 'Y')
-                                AND marker_input_detail.ratio > 0
-                                ".($this->colorFilter ? "AND marker_input.color = '".$this->colorFilter."'" :  "")."
-                                ".($this->groupBy == "size" && $this->sizeFilter ? "AND marker_input_detail.size = '".$this->sizeFilter."'" : "")."
-                                ".($this->selectedOrder ? "AND marker_input.act_costing_id = '".$this->selectedOrder."'" : "")."
-                            group by
-                                marker_input.id,
-                                marker_input_detail.so_det_id,
-                                form_cut.tgl_form_cut,
-                                form_cut.meja
-                        ) marker_cutting
-                    GROUP BY
-                        marker_cutting.act_costing_id,
-                        marker_cutting.color,
-                        marker_cutting.id_meja,
-                        marker_cutting.panel,
-                        ".($this->groupBy == 'size' ? ' marker_cutting.so_det_id, marker_cutting.size, ' : '')."
-                        marker_cutting.tgl_form_cut
-                    ORDER BY
-                        marker_cutting.act_costing_id,
-                        marker_cutting.color,
-                        marker_cutting.id_meja,
-                        marker_cutting.panel,
-                        ".($this->groupBy == 'size' ? ' marker_cutting.so_det_id, marker_cutting.size, ' : '')."
-                        marker_cutting.tgl_form_cut
-                ")
-            );
+                        where
+                            (marker_input.cancel IS NULL OR marker_input.cancel != 'Y')
+                            AND marker_input_detail.ratio > 0
+                            ".($this->colorFilter ? "AND marker_input.color = '".$this->colorFilter."'" :  "")."
+                            ".($this->panelFilter ? "AND marker_input.panel = '".$this->panelFilter."'" :  "")."
+                            ".($this->groupBy == "size" && $this->sizeFilter ? "AND master_sb_ws.size = '".$this->sizeFilter."'" : "")."
+                            ".($this->selectedOrder ? "AND marker_input.act_costing_id = '".$this->selectedOrder."'" : "")."
+                        group by
+                            marker_input.id,
+                            marker_input_detail.so_det_id,
+                            form_cut.id
+                    ) marker_cutting
+                GROUP BY
+                    marker_cutting.act_costing_id,
+                    marker_cutting.color,
+                    marker_cutting.id_meja,
+                    marker_cutting.panel,
+                    ".($this->groupBy == 'size' ? ' marker_cutting.so_det_id, marker_cutting.size, ' : '')."
+                    marker_cutting.tgl_form_cut
+                ORDER BY
+                    marker_cutting.act_costing_id,
+                    marker_cutting.color,
+                    marker_cutting.id_meja,
+                    marker_cutting.panel,
+                    ".($this->groupBy == 'size' ? ' marker_cutting.so_det_id, marker_cutting.size, ' : '')."
+                    marker_cutting.tgl_form_cut
+            ")
+        );
 
             $this->dailyOrderOutputs = $dailyOrderOutputSql;
 
