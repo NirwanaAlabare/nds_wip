e2579c464af94d1a8514a8b556de7c688934aa1f
Ramonez22 <ramonez.el.nino@gmail.com>
2025-08-07 11:58:09 +0700

    update rate blanket

--- resources/views/qc_inspect/proses_det_packinglist.blade.php
+++ resources/views/qc_inspect/proses_det_packinglist.blade.php
@@ -86,15 +86,14 @@
                             <!-- Rate Dropdown -->
                             <select name="rateSelect" id="rateSelect" class="form-select" required>
                                 <option value="" selected disabled>-- Select Rate --</option>
-                                <option value="0.5">0.5</option>
                                 <option value="1">1</option>
-                                <option value="1.5">1.5</option>
+                                <option value="0.5">1/2</option>
                                 <option value="2">2</option>
-                                <option value="2.5">2.5</option>
+                                <option value="2.5">2/3</option>
                                 <option value="3">3</option>
-                                <option value="3.5">3.5</option>
+                                <option value="3.5">3/4</option>
                                 <option value="4">4</option>
-                                <option value="4.5">4.5</option>
+                                <option value="4.5">4/5</option>
                                 <option value="5">5</option>
                             </select>
                         </div>
