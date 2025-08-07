a663059fc734b8817eec30eddf4adfa2e31e033c
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-08-07 08:41:01 +0700

    add undo output

--- resources/views/sewing/tools/tools.blade.php
+++ resources/views/sewing/tools/tools.blade.php
@@ -67,6 +67,15 @@
                         </div>
                     </a>
                 </div>
+                <div class="col-md-4">
+                    <a href="{{ route('undo-output') }}" class="home-item">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="text-sb mb-0"><i class="fa-solid fa-arrows-rotate"></i> Undo Output</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
---
+++ resources/views/sewing/tools/undo-output.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.index')
+
+@section('custom-link')
+<!-- DataTables -->
+<link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+
+<!-- Select2 -->
+<link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+@endsection
+
+@section('content')
+    <div class="card card-sb">
+        <div class="card-header">
+            <h5 class="card-title">
+                <i class="fa fa-solid fa-rotate fa-sm"></i> Undo Output
+            </h5>
+        </div>
+        <div class="card-body">
+            <label class="form-label">Kode Numbering :</label>
+            <textarea class="form-control" name="kode_numbering" id="kode_numbering" cols="30" rows="10"></textarea>
+            <div class="form-text">Contoh : <br>&nbsp;&nbsp;&nbsp;<b> {{ date("Y") }}_1_1</b><br>&nbsp;&nbsp;&nbsp;<b> {{ date("Y") }}_1_2</b><br>&nbsp;&nbsp;&nbsp;<b> {{ date("Y") }}_1_3</b></div>
+        </div>
+
+        <div class="card-footer border-1">
+            <button class="btn btn-sb btn-block" onclick="listTableReload()"><i class="fa fa-solid fa-search"></i> Search</button>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header bg-sb-secondary">
+            <h5 class="card-title">
+                <i class="fas fa-list fa-sm"></i> List Data
+            </h5>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered table-sm" id="list-table">
+                    <thead>
+                        <tr>
+                            <th>Kode</th>
+                            <th>WS</th>
+                            <th>Style</th>
+                            <th>Color</th>
+                            <th>Size</th>
+                            <th>Status</th>
+                            <th>Line</th>
+                            <th>Waktu</th>
+                            <th>Defect</th>
+                            <th>Alokasi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="card-footer border-1">
+            <button type="button" class="btn btn-sb-secondary btn-block" onclick="undoOutputSubmit()"><i class="fa fa-rotate"></i> UNDO</button>
+        </div>
+    </div>
+@endsection
+
+@section('custom-script')
+    <!-- DataTables & Plugins -->
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+
+    <!-- Select2 -->
+    <script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
+    <script>
+        $('.select2').select2()
+        $('.select2bs4').select2({
+            theme: 'bootstrap4'
+        })
+
+        let listTable = $("#list-table").DataTable({
+            ordering: false,
+            processing: true,
+            serverSide: true,
+            ajax: {
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url: '{{ route('get-output') }}',
+                dataType: 'json',
+                dataSrc: 'data',
+                scrollY: '400px',
+                data: function(d) {
+                    d.kode_numbering = $('#kode_numbering').val();
+                },
+            },
+            columns: [
+                { data: "kode_numbering" },
+                { data: "ws" },
+                { data: "style" },
+                { data: "color" },
+                { data: "size" },
+                { data: "status" },
+                { data: "sewing_line" },
+                { data: "updated_at" },
+                { data: "defect" },
+                { data: "allocation" }
+            ],
+            columnDefs: [
+                {
+                    targets: "_all",
+                    className: "text-nowrap",
+                    render: (data, type, row, meta) => data
+                },
+            ]
+        });
+
+        function listTableReload() {
+            listTable.ajax.reload();
+        }
+
+        function undoOutputSubmit() {
+            Swal.fire({
+                title: 'UNDO Output',
+                html: '<span class="text-danger"><b>Critical</b></span> <br> Yakin akan UNDO OUTPUT ini?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'UNDO',
+                cancelButtonText: 'BATAL',
+                confirmButtonColor: "#dc3545"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById("loading").classList.remove("d-none");
+
+                    $.ajax({
+                        url: "{{ route('undo-output-submit') }}",
+                        type: "post",
+                        data: {
+                            kode_numbering: $("#kode_numbering").val()
+                        },
+                        dataType: "json",
+                        success: function (res) {
+                            document.getElementById("loading").classList.add("d-none");
+
+                            console.log(res);
+
+                            if (res.length > 0) {
+                                let message = res.join("<br>");
+
+                                Swal.fire({
+                                    icon: 'info',
+                                    title: 'Info',
+                                    html: message,
+                                    showCancelButton: false,
+                                    showConfirmButton: true,
+                                    confirmButtonText: 'Oke',
+                                    confirmButtonColor: "#082149",
+                                });
+
+                                listTableReload();
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    </script>
+@endsection
