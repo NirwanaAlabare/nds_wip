4df463fedc68dc61399df6fc0a293502c7e17e54
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-08-18 09:07:04 +0700

    adjusting

--- app/Http/Controllers/PPICDashboardController.php
+++ app/Http/Controllers/PPICDashboardController.php
@@ -36,7 +36,7 @@ group by created_by, so_det_id
 ) a
 left join master_sb_ws m on a.so_det_id = m.id_so_det
 left join master_size_new msn on m.size = msn.size
-left join (select id_so_det, group_concat(distinct(po)) list_po from ppic_master_so where tgl_shipment >= '$tgl_shipment' group by id_so_det) b on a.so_det_id = b.id_so_det
+left join (select id_so_det, group_concat(distinct(po)) list_po from ppic_master_so where tgl_shipment >= '$tgl_filter' group by id_so_det) b on a.so_det_id = b.id_so_det
 group by a.created_by, m.ws, m.color, m.size, m.dest
 order by a.created_by asc, ws asc, color asc, dest asc, urutan asc
             ");
