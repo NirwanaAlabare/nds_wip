2a3f8ec49f53e6ee07cc880f25398e7919b97bfd
it-nag <ramon@ptnag.com>
2025-10-01 08:20:50 +0700

    add role access management

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -273,7 +273,7 @@
                             </div>
                         @endrole
 
-                        @role('accounting')
+                        @role('management')
                             <div class="col-lg-2 col-md-3 col-sm-6">
                                 <a href="{{ route('dashboard-mgt-report') }}" class="home-item">
                                     <div class="card h-100">
