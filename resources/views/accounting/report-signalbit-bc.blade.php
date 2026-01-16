06d58c8594e94cf4ebd22f21d40f6e7bd75e39a0
Druhiyat24 <dederuhiyat24@gmail.com>
2026-01-16 08:28:54 +0700

    adjust

--- resources/views/accounting/report-signalbit-bc.blade.php
+++ resources/views/accounting/report-signalbit-bc.blade.php
@@ -15,7 +15,7 @@
     <form action="{{ route('export_excel_pemasukanroll') }}" method="get">
         <div class="card card-sb">
             <div class="card-header">
-                <h5 class="card-title fw-bold mb-0"><i class="fas fa-file-alt fa-sm"></i>Data BC SignalBit</h5>
+                <h5 class="card-title fw-bold mb-0"><i class="fas fa-file-alt fa-sm"></i> Data BC SignalBit</h5>
             </div>
             <div class="card-body">
                 <div class="d-flex align-items-end gap-3 mb-3">
