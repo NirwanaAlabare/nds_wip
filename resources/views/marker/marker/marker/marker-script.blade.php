afd53769732e9c0760458ce9cf0d802f11c8b25c
it-nag <ramon@ptnag.com>
2026-03-09 15:00:39 +0700

    fix marker view (cancel global datatable effect)

--- resources/views/marker/marker/marker/marker-script.blade.php
+++ resources/views/marker/marker/marker/marker-script.blade.php
@@ -1,3 +1,7 @@
+<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 <script>
 
     document.addEventListener("DOMContentLoaded", () => {
