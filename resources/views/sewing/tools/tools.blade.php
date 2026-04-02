c5fb1162c6f48c7cab5805d9555b1a36f1fb5dfb
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2026-04-02 13:41:04 +0700

    adjust modify output packing po

--- resources/views/sewing/tools/tools.blade.php
+++ resources/views/sewing/tools/tools.blade.php
@@ -147,6 +147,15 @@
                         </div>
                     </a>
                 </div>
+                <div class="col-md-4">
+                    <a href="{{ route('modify-packing-po') }}" class="home-item">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="text-sb mb-0"><i class="fa-solid fa-box"></i> Modify Packing PO</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
