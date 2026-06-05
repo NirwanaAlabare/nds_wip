8db73c8076ea0dca58396a3a5654f0ab1763be4c
it-nag <152240455+it-nag@users.noreply.github.com>
2026-06-05 14:18:01 +0700

    add inject mutasi output on report finishing

--- app/Http/Controllers/Sewing/FinishingMendingReportController.php
+++ app/Http/Controllers/Sewing/FinishingMendingReportController.php
@@ -46,7 +46,9 @@ class FinishingMendingReportController extends Controller
               AND a.created_at <= ?
               $buyerFilter
             GROUP BY ms.supplier, ac.kpno, ac.styleno, sd.color, sd.size, msn.urutan
-            ORDER BY buyer ASC, ws ASC, color ASC, msn.urutan ASC
+            UNION ALL
+            select buyer, ws, styleno style, color, size, COALESCE(defect_mending_f,0) jumlah from signalbit_erp.inject_mutasi_sewing where type_saldo = 'FINISHING' AND defect_mending_f > 0 AND tgl_saldo >= '$startDate' AND tgl_saldo <= '$endDate'
+            ORDER BY buyer ASC, ws ASC, color ASC
         ";
 
         return DB::connection('mysql_sb')->select($sql, $bindings);
