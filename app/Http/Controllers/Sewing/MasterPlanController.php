d60a70546780abebedc2b331704fd634d8d15cc5
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2026-07-08 10:42:01 +0700

    adjust sewing module

--- app/Http/Controllers/Sewing/MasterPlanController.php
+++ app/Http/Controllers/Sewing/MasterPlanController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Yajra\DataTables\Facades\DataTables;
 use Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 use Carbon\Carbon;
 use DB;
 
@@ -326,6 +327,73 @@ class MasterPlanController extends Controller
             'table' => '',
             'additional' => [],
         );
+    }
+
+    public function importMasterPlanPreview(Request $request)
+    {
+        $file = $request->file('file');
+
+        $rows = Excel::toArray([], $file)[0];
+
+        $data = [];
+
+        // Skip header row (start from the second row)
+        $dataRows = array_slice($rows, 1);
+
+        foreach ($dataRows as $row)
+        {
+            if (empty(array_filter($row))) {
+                continue; // Skip empty rows
+            }
+            $orderInfo = DB::connection("mysql_sb")->select("
+                SELECT
+                    mastersupplier.Supplier as buyer,
+                    act_costing.id as id_cost,
+                    act_costing.kpno as ws,
+                    act_costing.styleno as style,
+                    so_det.color,
+                    GROUP_CONCAT(so_det.size) sizes,
+                    GROUP_CONCAT(so_det.id) so_det_ids
+                FROM
+                    so_det
+                    LEFT JOIN so ON so.id = so_det.id_so
+                    LEFT JOIN act_costing ON act_costing.id = so.id_cost
+                    LEFT JOIN mastersupplier ON mastersupplier.Id_Supplier = act_costing.id_buyer
+                WHERE
+                    act_costing.kpno = '".$row[2]."' and
+                    so_det.color LIKE '%".$row[3]."%'
+                GROUP BY
+                    act_costing.id
+                LIMIT 1
+            ");
+
+            $excelDate = $row[0];
+            $convertedDate = Date::excelToDateTimeObject(intval($excelDate));
+            $formattedDate = Carbon::instance($convertedDate)->format('Y-m-d');
+
+            $data[] = [
+                "id_plan" => str_replace("-", "", $formattedDate),
+                "sewing_line" => $row[1],
+                "tgl_plan" => $formattedDate,
+                "tgl_input" => Carbon::now(),
+                "id_ws" => $orderInfo && $orderInfo[0] ? $orderInfo[0]->id_cost : null,
+                "ws" => $orderInfo && $orderInfo[0] ? $orderInfo[0]->ws : $row[2],
+                "color" => $orderInfo && $orderInfo[0] ? $orderInfo[0]->color : $row[3],
+                "smv" => $row[4],
+                "jam_kerja" => $row[5],
+                "man_power" => $row[6],
+                "plan_target" => $row[7],
+                "target_effy" => $row[8],
+                "create_by" => Auth::user()->username,
+                "cancel" => "N"
+            ];
+        }
+
+        return response()->json([
+            "status" => 200,
+            "message" => 'Preview data berhasil diambil',
+            "data" => $data
+        ]);
     }
 
     public function importMasterPlan(Request $request)
