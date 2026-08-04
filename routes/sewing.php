92769bf293cd778e6bfa4d2a8ca4bedf252afab2
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2026-08-04 09:54:26 +0700

    add undo packing po list restore action button

--- routes/sewing.php
+++ routes/sewing.php
@@ -184,6 +184,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/undo-packing-po-list', 'undoPackingPoList')->name("undo-packing-po-list");
         Route::get('/get-undo-packing-po-list', 'getUndoPackingPoList')->name("get-undo-packing-po-list");
         Route::get('/export-undo-packing-po-list', 'exportUndoPackingPoList')->name("export-undo-packing-po-list");
+        Route::post('/restore-packing-po', 'restorePackingPo')->name("restore-packing-po");
 
         // Reject IN OUT
         Route::get('/undo-reject', 'undoReject')->name("undo-reject");
