5788501760d66b6febc0dcb8751808be049ebcd3
alialfi <45139879+alialfi@users.noreply.github.com>
2026-09-02 16:15:49 +0700

    tes data rekap pemasukan

--- app/Services/ReportBc/PemasukanService.php
+++ app/Services/ReportBc/PemasukanService.php
@@ -243,7 +243,7 @@ class PemasukanService
                 ->join('mastercontents as mcnt', 'swd.id_contents', '=', 'mcnt.id')
                 ->join('mastersupplier as d', 'a.id_supplier', '=', 'd.id_supplier')
                 ->where('a.cancel', 'N')
-                ->where('a.jenis_dok', '!=', 'INHOUSE')
+                // ->where('a.jenis_dok', '!=', 'INHOUSE')
                 ->where('a.bpbno', 'not like', 'FG%')
                 ->whereBetween($dateField, [$fromDate, $toDate]);
 
@@ -271,11 +271,11 @@ class PemasukanService
                 ->join('so', 'sod.id_so', '=', 'so.id')
                 ->join('act_costing as ac', 'so.id_cost', '=', 'ac.id')
                 ->where('a.cancel', 'N')
-                ->where('a.jenis_dok', '!=', 'INHOUSE')
+                // ->where('a.jenis_dok', '!=', 'INHOUSE')
                 ->where('a.bpbno', 'like', 'FG%')
                 ->whereBetween($dateField, [$fromDate, $toDate])
                 ->select($selectData(
-                    "'N/A'",
+                    "MAX(a.jenis_dok) as jenis_dokumen",
                     "a.bcdate",
                     "ac.kpno",
                     "s.itemname",
@@ -284,6 +284,7 @@ class PemasukanService
                 ))
                 ->groupBy('ac.kpno');
         }
+
 
         if ($queryBahanBaku && $queryBarangJadi) {
             $unionQuery = $queryBahanBaku->unionAll($queryBarangJadi);
