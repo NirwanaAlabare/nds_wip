faca44b0139336031b2f9a524c4179a5ae7b3ffb
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2026-09-11 15:14:00 +0700

    fix roll qty - consider qty penerimaan

--- app/Services/CuttingService.php
+++ app/Services/CuttingService.php
@@ -644,7 +644,7 @@ class CuttingService
                             $scannedItem
                         ]);
 
-                        if ($scannedItem->qty != $rollQty) {
+                        if ($qty && $scannedItem->qty != $rollQty) {
                             $scannedItem->qty = $rollQty;
                         } else {
                             $currentRoll = $roll->where("id_roll", $rollId)->first();
@@ -654,14 +654,14 @@ class CuttingService
                                     $scannedItem->qty = $currentRoll->sisa_kain;
                                 }
 
-                                // // Checking new penerimaan after last input
-                                // $newPenerimaan = PenerimaanCutting::where("id_roll", $rollId)->
-                                //     where("created_at", ">", $currentRoll->created_at)->
-                                //     sum("qty_konv");
+                                // Checking new penerimaan after last input
+                                $newPenerimaan = PenerimaanCutting::where("id_roll", $rollId)->
+                                    where("created_at", ">", $currentRoll->created_at)->
+                                    sum("qty_konv");
 
-                                // if ($newPenerimaan > 0) {
-                                //     $scannedItem->qty += $newPenerimaan;
-                                // }
+                                if ($newPenerimaan > 0) {
+                                    $scannedItem->qty += $newPenerimaan;
+                                }
                             }
                         }
 
