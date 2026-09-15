cb4ba7e31183512f1e9087a5b711074a19c917d6
alialfi <45139879+alialfi@users.noreply.github.com>
2026-09-15 11:27:03 +0700

    Update PemasukanService.php

--- app/Services/ReportBc/PemasukanService.php
+++ app/Services/ReportBc/PemasukanService.php
@@ -295,7 +295,7 @@ class PemasukanService
             $queryFgStokScan = $mysql_sb->table('laravel_nds.fg_stok_bpb_scan as a')
                 ->leftJoin('laravel_nds.master_sb_ws as m', 'a.id_so_det', '=', 'm.id_so_det')
                 ->where('a.cancel', '!=', 'Y')
-                ->where('a.tgl_terima <', $toDate)
+                ->where('a.tgl_terima', '<' , $toDate)
                 ->whereNotIn('a.sumber_pemasukan', ['EKSPEDISI', 'MUTASI INTERNAL'])
                 ->select([
                     DB::raw("'INHOUSE' as jenis_dokumen"),
