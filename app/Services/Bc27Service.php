8e1ad361f5b48987952e7eef90cfc074919d062a
alialfi <45139879+alialfi@users.noreply.github.com>
2026-09-15 16:18:09 +0700

    bc27 service

--- app/Services/Bc27Service.php
+++ app/Services/Bc27Service.php
@@ -1705,15 +1705,15 @@ class Bc27Service
             if ($responseCeisa['successful']) {
                 foreach ($bpbs as $no_bpb) {
 
-                    $data_kantor = $db->table('master_kantor')
-                                    ->where('kode', $draft['kodeKantor'])
-                                    ->get();
+                    // $data_kantor = $db->table('master_kantor')
+                    //                 ->where('kode', $draft['kodeKantor'])
+                    //                 ->get();
 
-                    //kode kantor bandung
-                    $kantor = 60; 
-                    if($data_kantor){
-                        $kantor = $data_kantor->id;
-                    }
+                    // //kode kantor bandung
+                    // $kantor = 60; 
+                    // if($data_kantor){
+                    //     $kantor = $data_kantor->id;
+                    // }
 
                     $updated = $db->table('bppb')
                         ->where(function($q) use ($no_bpb) {
@@ -1723,7 +1723,7 @@ class Bc27Service
                             'nomor_aju'   => $nomorAju,
                             'tanggal_aju' => date('Y-m-d'),
                             'bcdate'      => date('Y-m-d'),
-                            'kode_kantor' => $kantor,
+                            // 'kode_kantor' => $kantor,
                         ]);
 
                     if ($updated === 0) {
