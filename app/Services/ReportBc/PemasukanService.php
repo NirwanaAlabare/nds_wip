ea58d23cce29b7a88193171c99708e62f0322e3a
alialfi <45139879+alialfi@users.noreply.github.com>
2026-09-15 09:50:09 +0700

    naming

--- app/Services/ReportBc/PemasukanService.php
+++ app/Services/ReportBc/PemasukanService.php
@@ -1557,7 +1557,7 @@ class PemasukanService
             'Nama ' . ($jenis == 'pemasukan' ? 'Pengirim' : 'Penerima'),
             'Nomor BPB',
             'Tanggal BPB',
-            'ID Item',
+            'WS',
             'Uraian Barang',
             'Jenis Satuan',
             'Jumlah Satuan',
