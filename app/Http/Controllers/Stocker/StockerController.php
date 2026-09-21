2c0a307d31ecdce00c80f46f644ec289c96553c4
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2026-09-21 13:35:51 +0700

    take qty cut instead of qty ply group

--- app/Http/Controllers/Stocker/StockerController.php
+++ app/Http/Controllers/Stocker/StockerController.php
@@ -921,7 +921,7 @@ class StockerController extends Controller
 
                 // Stocker Spec
                 // $qty = ($i == ($ratio - 1) && $formDetailOutput) ? (($ratio < 1 ? 0 : $qtyPlyGroup) + ($formDetailOutput->qty_output_aktual - ($ratio * $qtyPlyGroup))) : ($ratio < 1 ? 0 : $qtyPlyGroup);
-                $qty = ($i == ($ratio - 1) && $formDetailOutput) ? (($ratio < 1 ? 0 : $qtyPlyGroup)) : ($ratio < 1 ? 0 : $qtyPlyGroup);
+                $qty = ($i == ($ratio - 1) && $formDetailOutput) ? (($ratio < 1 ? 0 : $qtyCut)) : ($ratio < 1 ? 0 : $qtyCut);
                 $stockerId = $checkStocker ? $checkStocker->id_qr_stocker : "STK-" . ($stockerCount + $i);
                 $cumRangeAwal = $cumRangeAkhir + 1;
                 $cumRangeAkhir = $cumRangeAkhir + ($ratio < 1 ? null : $qty);
